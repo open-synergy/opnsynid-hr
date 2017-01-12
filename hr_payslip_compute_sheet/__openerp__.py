b63bdd93043eefdb6c8780c4f6ca9ab1e4ec661d
mike <viriyananda.michael@gmail.com>
2017-01-12 15:51:54 +0700

    Adding new module hr_payslip_compute_sheet
    Revision

---
+++ hr_payslip_compute_sheet/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "HR Payslip Compute Sheet",
+    "version": "8.0.1.0.0",
+    "summary": "Adds wizard to compute sheet",
+    "category": "Human Resources",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": ["hr_payroll"],
+    "data": [
+        "wizards/hr_compute_sheet_views.xml",
+        "views/hr_payslip_views.xml",
+        "data/ir_values_data.xml"
+    ],
+}
