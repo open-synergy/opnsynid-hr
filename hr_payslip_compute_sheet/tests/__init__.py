1c7405e8bd358219031b968c3a60655667e5cdbf
mike <viriyananda.michael@gmail.com>
2017-01-30 10:50:01 +0700

    Adding Unittest

--- hr_payslip_compute_sheet/tests/__init__.py
+++ hr_payslip_compute_sheet/tests/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2016 OpenSynergy Indonesia
+# Copyright 2016 Michael Viriyananda
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from . import hr_compute_sheet
+from . import test_hr_payslip
