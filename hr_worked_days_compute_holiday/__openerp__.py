87323546d48542cdce87fe9304f0de85eb093257
mike <viriyananda.michael@gmail.com>
2017-02-06 14:43:25 +0700

    Adding new module hr_worked_days_compute_holiday
    Modifiy method get_worked_day_lines
    
    Fixing travis error
    
    Revision
    
    Fixing Travis Error

---
+++ hr_worked_days_compute_holiday/__openerp__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "HR Worked Days Compute Holiday",
+    "version": "8.0.1.0.0",
+    "category": "Human Resources",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "hr_payroll",
+        "hr_holidays_compute_days"
+    ],
+    "data": [],
+}
