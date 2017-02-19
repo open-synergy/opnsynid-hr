5cc94fcc8d1fa461f1377d78096c8ca41ee6f168
Andhitia Rama <andhitia.r@gmail.com>
2017-02-19 19:14:49 +0700

    Adjustment to OCA guidelines. Add OpenSynergy Indonesia brand

--- hr_attendance_computation/__init__.py
+++ hr_attendance_computation/__init__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-# © 2011 Domsense srl (<http://www.domsense.com>)
-# © 2011-15 Agile Business Group sagl (<http://www.agilebg.com>)
-# © 2016 OpenSynergy Indonesia (<https://opensynergy-indonesia.com>)
+# Copyright 2011 Domsense srl (<http://www.domsense.com>)
+# Copyright 2011-15 Agile Business Group sagl (<http://www.agilebg.com>)
+# Copyright 2017 OpenSynergy Indonesia (<https://opensynergy-indonesia.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 
-from . import models, tests
+from . import models
--- hr_attendance_computation/models/res_company.py
+++ hr_attendance_computation/models/res_company.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Copyright 2011 Domsense srl (<http://www.domsense.com>)
 # Copyright 2011-15 Agile Business Group sagl (<http://www.agilebg.com>)
-# Copyright 2017 OpenSynergy Indonesia (<https://www.opensynergy-indonesia.com>)
+# Copyright 2017 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from __future__ import division
