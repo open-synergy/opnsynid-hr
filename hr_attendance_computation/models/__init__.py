3760791f1ea0aab493471d936dcb3b8745307e41
Andhitia Rama <andhitia.r@gmail.com>
2017-02-19 16:46:21 +0700

    Split res.company obj

--- hr_attendance_computation/models/__init__.py
+++ hr_attendance_computation/models/__init__.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
-# © 2011 Domsense srl (<http://www.domsense.com>)
-# © 2011-15 Agile Business Group sagl (<http://www.agilebg.com>)
+# Copyright 2011 Domsense srl (<http://www.domsense.com>)
+# Copyright 2011-15 Agile Business Group sagl (<http://www.agilebg.com>)
+# Copyright 2017 OpenSynergy Indonesia (<https://www.opensynergy-indonesia.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 
-from . import hr_attendance, hr_contract, resource_resource
+from . import res_company, hr_attendance, hr_contract, resource_resource
