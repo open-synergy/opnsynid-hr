5ba5b9d9f4bcf832252b3a7010cb853127a05b31
Andhitia Rama <andhitia.r@gmail.com>
2017-09-02 15:06:14 +0700

    [8.0.1.0.0] hr_contrib_register_partner

---
+++ hr_contrib_register_partner/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Show Partner on Contribution Register",
+    "version": "8.0.1.0.0",
+    "category": "Human Resources",
+    "website": "https://opensynergy-indonesia/",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "hr_payroll",
+    ],
+    "data": [
+        "views/hr_contribution_register_views.xml",
+    ],
+}
