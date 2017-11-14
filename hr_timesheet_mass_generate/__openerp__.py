7996e26b54a354e080cc05e46223c2d520f5862b
mike <viriyananda.michael@gmail.com>
2017-11-14 22:23:06 +0700

    Adding new module hr_timesheet_mass_generate

---
+++ hr_timesheet_mass_generate/__openerp__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Mass Generate Timesheet",
+    "version": "8.0.1.0.0",
+    "category": "Human Resources",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": ["hr_timesheet_sheet"],
+    "data": [
+        "wizards/hr_timesheet_mass_generate.xml"
+    ],
+}
