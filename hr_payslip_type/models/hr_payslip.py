abc08959a623597d64242311c3267d61d8335610
mike <viriyananda.michael@gmail.com>
2017-11-16 21:15:30 +0700

    [8.0.1.0.0] hr_payslip_type

---
+++ hr_payslip_type/models/hr_payslip.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, api, fields
+
+
+class HrPayslip(models.Model):
+    _inherit = "hr.payslip"
+
+    def _default_payslip_type(self):
+        return self.env['hr.payslip.type'].search([])[:1]
+
+    payslip_type_id = fields.Many2one(
+        string="Payslip Type",
+        comodel_name="hr.payslip.type",
+        default=_default_payslip_type
+    )
