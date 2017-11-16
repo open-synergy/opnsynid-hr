bfc57f5df48c0a36ad2e4af44dbcc4740b0891d7
mike <viriyananda.michael@gmail.com>
2017-11-16 21:15:30 +0700

    [8.0.1.0.0] hr_payslip_type

---
+++ hr_payslip_type/wizards/hr_payroll_payslips_by_employees.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import models, api
+import logging
+
+
+_logger = logging.getLogger(__name__)
+
+
+class HrPayslipEmployees(models.TransientModel):
+    _inherit = "hr.payslip.employees"
+
+    @api.multi
+    def compute_sheet(self):
+        res = super(HrPayslipEmployees, self).compute_sheet()
+        obj_hr_payslip_run = self.env["hr.payslip.run"]
+        payslip_run = obj_hr_payslip_run.browse(
+            self.env.context['active_id'])
+        payslip_type =\
+            payslip_run.payslip_type_id and \
+            payslip_run.payslip_type_id.id or False
+
+        payslip_run.slip_ids.write({
+            'payslip_type_id': payslip_type
+        })
+        payslip_run.slip_ids.compute_sheet()
+        return res
