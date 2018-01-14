f7b1fb33972709852f5ed3d90c124bc2a830bd09
Andhitia Rama <andhitia.r@gmail.com>
2018-01-14 17:32:50 +0700

    [8.0.1.0.0] hr_employee_training_analytic

---
+++ hr_employee_training_analytic/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Employee Training - Analytic",
+    "version": "8.0.1.0.0",
+    "category": "Human Resource",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "hr_employee_training",
+        "account_accountant",
+    ],
+    "data": [
+        "views/hr_training_views.xml",
+    ],
+}
