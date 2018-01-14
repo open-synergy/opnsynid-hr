e5f11cef1760107f41bb498c986940d275d3cde3
Andhitia Rama <andhitia.r@gmail.com>
2018-01-14 09:42:42 +0700

    [LINT] hr_payslip_type

--- hr_payslip_type/models/__init__.py
+++ hr_payslip_type/models/__init__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import (
-	hr_payslip_type,
-	hr_payslip,
-	hr_payslip_run
+    hr_payslip_type,
+    hr_payslip,
+    hr_payslip_run
 )
