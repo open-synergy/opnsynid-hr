0449931fab4db7843c9c231c7bd7aa503e7051b9
Andhitia Rama <andhitia.r@gmail.com>
2018-01-14 09:42:42 +0700

    [hr_payslip_type] Fix lint

--- hr_payslip_type/models/hr_payslip.py
+++ hr_payslip_type/models/hr_payslip.py
@@ -2,7 +2,7 @@
 # Copyright 2017 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import models, api, fields
+from openerp import models, fields
 
 
 class HrPayslip(models.Model):
