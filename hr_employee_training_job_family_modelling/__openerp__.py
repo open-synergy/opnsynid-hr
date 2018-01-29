848790a7021420820d735cbb7d82bfdc0da6d33e
Andhitia Rama <andhitia.r@gmail.com>
2018-01-29 16:30:28 +0700

    [8.0.1.0.0] hr_employee_training_job_family_modelling

---
+++ hr_employee_training_job_family_modelling/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Employee Training - Job Family Modelling Integration",
+    "version": "8.0.1.0.0",
+    "category": "Human Resource",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "hr_employee_training",
+        "hr_job_family_modelling",
+    ],
+    "data": [
+        "views/hr_training_views.xml",
+    ],
+    "auto_install": True,
+}
