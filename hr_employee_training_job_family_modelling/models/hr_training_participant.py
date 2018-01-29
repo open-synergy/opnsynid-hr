7e7ed56081a24d7c9cd6f6221d9b8ca63c731c03
Andhitia Rama <andhitia.r@gmail.com>
2018-01-29 16:30:28 +0700

    [8.0.1.0.0] hr_employee_training_job_family_modelling

---
+++ hr_employee_training_job_family_modelling/models/hr_training_participant.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields, api
+
+
+class HrTrainingParticipant(models.Model):
+    _inherit = "hr.training_partisipant"
+
+    job_grade_id = fields.Many2one(
+        string="Job Grade",
+        comodel_name="hr.job_grade",
+    )
+    job_family_grade_id = fields.Many2one(
+        string="Job Family Grade",
+        comodel_name="hr.job_family_grade",
+    )
+
+    @api.onchange(
+        "partisipant_id",
+    )
+    def onchange_job_grade_id(self):
+        self.job_grade_id = False
+        if self.partisipant_id:
+            self.job_grade_id = self.partisipant_id.job_grade_id
+
+    @api.onchange(
+        "partisipant_id",
+    )
+    def onchange_job_family_grade_id(self):
+        self.job_family_grade_id = False
+        if self.partisipant_id:
+            self.job_family_grade_id = self.partisipant_id.job_family_grade_id
