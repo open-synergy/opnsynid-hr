4725f795facfd0f72500f19c88f6cbf7b1c897a3
Andhitia Rama <andhitia.r@gmail.com>
2018-03-18 12:59:44 +0700

    [8.0.1.0.0] hr_employee_training_organization_unit

---
+++ hr_employee_training_organization_unit/models/hr_training.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields, api
+
+
+class HrTraining(models.Model):
+    _inherit = "hr.training"
+
+    organization_unit_id = fields.Many2one(
+        string="Organization Unit",
+        comodel_name="hr.department",
+        readonly=True,
+        states={
+            "draft": [
+                ("readonly", False),
+            ],
+        },
+    )
+
+    @api.onchange(
+        "request_by_id",
+    )
+    def onchange_organization_unit_id(self):
+        self.organization_unit_id = False
+        if self.request_by_id:
+            self.organization_unit_id = self.request_by_id.organization_unit_id
---
+++ hr_employee_training_organization_unit/models/hr_training_participant.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields, api
+
+
+class HrTrainingParticipant(models.Model):
+    _inherit = "hr.training_partisipant"
+
+    organization_unit_id = fields.Many2one(
+        string="Organization Unit",
+        comodel_name="hr.department",
+    )
+
+    @api.onchange(
+        "partisipant_id",
+    )
+    def onchange_organization_unit_id(self):
+        self.organization_unit_id = False
+        if self.partisipant_id:
+            self.organization_unit_id = \
+                self.partisipant_id.organization_unit_id
