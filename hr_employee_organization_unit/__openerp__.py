b0afbe524d2be3fc3fa958cc20e18e436d521422
Andhitia Rama <andhitia.r@gmail.com>
2018-03-18 12:48:05 +0700

    [8.0.1.2.0] hr_employee_organization_unit
    * Add group by organization unit on employee search view

--- hr_employee_organization_unit/__openerp__.py
+++ hr_employee_organization_unit/__openerp__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Human Resource - Employee Organization Unit",
-    "version": "8.0.1.1.0",
+    "version": "8.0.1.2.0",
     "category": "Human Resource",
     "website": "https://opensynergy-indonesia.com",
     "author": "OpenSynergy Indonesia",
