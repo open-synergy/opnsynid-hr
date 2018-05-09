54b18238a6e937b2352c6e8adebf71df44197abd
Andhitia Rama <andhitia.r@gmail.com>
2018-05-09 21:40:25 +0700

    [8.0.1.0.0] hr_employee_training_experience

---
+++ hr_employee_training_experience/models/hr_certification.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields, api
+from openerp.exceptions import Warning as UserError
+from openerp.tools.translate import _
+
+
+class HrCertification(models.Model):
+    _inherit = "hr.certification"
+
+    training_category_id = fields.Many2one(
+        string="Training Category",
+        comodel_name="hr.training_category",
+        domain=[
+            ("type", "=", "normal"),
+        ],
+        ondelete="cascade",
+    )
+    participant_id = fields.Many2one(
+        string="Participant",
+        comodel_name="hr.training_partisipant",
+    )
+    training_id = fields.Many2one(
+        string="Training",
+        comodel_name="hr.training",
+        related="participant_id.training_id",
+        store=True,
+    )
+
+    @api.multi
+    def unlink(self):
+        _super = super(HrCertification, self)
+        for certification in self:
+            if certification.participant_id:
+                str_warning = _("You can not delete certification that "
+                                "link into training participant")
+                raise UserError(str_warning)
+        return _super.unlink()
---
+++ hr_employee_training_experience/models/hr_training_participant.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from datetime import datetime
+from openerp import models, api
+
+
+class HrTrainingParticipant(models.Model):
+    _inherit = "hr.training_partisipant"
+
+    @api.multi
+    def button_accomplish(self):
+        _super = super(HrTrainingParticipant, self)
+        _super.button_accomplish()
+
+        for participant in self:
+            participant._create_certification()
+
+    @api.multi
+    def _create_certification(self):
+        self.ensure_one()
+        obj_certification = self.env["hr.certification"]
+        obj_certification.create(
+            self._prepare_create_certification())
+
+    @api.multi
+    def button_reset(self):
+        _super = super(HrTrainingParticipant, self)
+        _super.button_reset()
+        for participant in self:
+            participant._delete_certification()
+
+    @api.multi
+    def _prepare_create_certification(self):
+        self.ensure_one()
+        training = self.training_id
+        date_start = datetime.strptime(
+            self.training_id.date_start,
+            "%Y-%m-%d %H:%M:%S").strftime("%Y-%m-%d")
+        date_end = datetime.strptime(self.training_id.date_end,
+                                     "%Y-%m-%d %H:%M:%S").strftime("%Y-%m-%d")
+        return {
+            "name": training.name,
+            "employee_id": self.partisipant_id.id,
+            "start_date": date_start,
+            "end_date": date_end,
+            "expire": False,
+            "training_category_id": training.category_id.id,
+            "participant_id": self.id,
+        }
+
+    @api.multi
+    def _delete_certification(self):
+        self.ensure_one()
+        domain = self._prepare_certification_domain()
+        obj_certification = self.env["hr.certification"]
+        certifications = obj_certification.search(domain)
+        certifications.write({"participant_id": False})
+        certifications.unlink()
+
+    @api.multi
+    def _prepare_certification_domain(self):
+        self.ensure_one()
+        return [
+            ("participant_id", "=", self.id),
+        ]
