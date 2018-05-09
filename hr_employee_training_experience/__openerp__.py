e0e085e6784b211b36a7944159661849b09ed298
Andhitia Rama <andhitia.r@gmail.com>
2018-05-09 21:40:25 +0700

    [8.0.1.0.0] hr_employee_training_experience

---
+++ hr_employee_training_experience/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Employee Training - Experience Integration",
+    "version": "8.0.1.0.0",
+    "category": "Human Resource",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "hr_employee_training",
+        "hr_experience",
+    ],
+    "data": [
+        "views/hr_certification_views.xml",
+    ],
+}
