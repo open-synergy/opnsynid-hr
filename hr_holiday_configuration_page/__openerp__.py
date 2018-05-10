8f66d82faab983ebb53005af1dc7c158c314c6fd
Andhitia Rama <andhitia.r@gmail.com>
2018-05-10 23:46:31 +0700

    [8.0.1.0.0] hr_holiday_configuration_page

---
+++ hr_holiday_configuration_page/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "HR Holiday - Configuration Page",
+    "version": "8.0.1.0.0",
+    "category": "Hidden",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "auto_install": True,
+    "depends": [
+        "hr_holidays",
+    ],
+    "data": [
+        "menu.xml",
+        "views/hr_holiday_config_setting_views.xml",
+    ],
+}
