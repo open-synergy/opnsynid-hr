4d1f0054cc30e74834a98416046207121d5caf42
Andhitia Rama <andhitia.r@gmail.com>
2018-05-25 23:16:29 +0700

    [8.0.1.0.2] hr_worked_days_compute_holiday
    Migrate to new api

--- hr_worked_days_compute_holiday/__openerp__.py
+++ hr_worked_days_compute_holiday/__openerp__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Public Holiday Computation on Worked Days",
-    "version": "8.0.1.0.1",
+    "version": "8.0.1.0.2",
     "category": "Human Resources",
     "website": "https://opensynergy-indonesia.com",
     "author": "OpenSynergy Indonesia",
