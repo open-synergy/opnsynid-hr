e3adebc9503a9b9db0643995e70ee2d064230229
Andhitia Rama <andhitia.r@gmail.com>
2018-05-25 14:38:34 +0700

    [8.0.1.0.0] hr_attendance_configuration_page

---
+++ hr_attendance_configuration_page/models/res_config.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields, api
+
+
+class ResConfig(models.TransientModel):
+    _name = "hr.attendance_config_setting"
+    _inherit = "res.config.settings"
+
+    @api.model
+    def _default_company_id(self):
+        return self.env.user.company_id.id
+
+    company_id = fields.Many2one(
+        string="Company",
+        comodel_name="res.company",
+        required=True,
+        default=lambda self: self._default_company_id(),
+    )
