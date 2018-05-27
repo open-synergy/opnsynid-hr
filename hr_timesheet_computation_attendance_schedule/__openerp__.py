479132386c30dc293c96f5afe92333b5a1366e48
Andhitia Rama <andhitia.r@gmail.com>
2018-05-27 23:29:45 +0700

    [8.0.1.0.0] hr_timesheet_computation_attendance_schedule

---
+++ hr_timesheet_computation_attendance_schedule/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Timesheet Computation Integration With Attendance Schedule",
+    "version": "8.0.1.0.0",
+    "category": "Human Resource",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "hr_timesheet_computation",
+        "hr_timesheet_attendance_schedule",
+    ],
+    "data": [
+    ],
+}
