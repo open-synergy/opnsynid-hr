06d1ae616de62e28fced0898835e6d28fc4e1656
Andhitia Rama <andhitia.r@gmail.com>
2018-06-06 11:55:19 +0700

    [8.0.1.0.0] hr_attendance_security

---
+++ hr_attendance_security/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Attendance Security",
+    "version": "8.0.1.0.0",
+    "category": "Human Resources",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "hr_attendance",
+    ],
+    "data": [
+        "security/ir_module_category_data.xml",
+        "security/res_groups_data.xml",
+        "security/ir.model.access.csv",
+        "security/ir_model_access_data.xml",
+        "security/ir_rule_data.xml",
+    ],
+}
