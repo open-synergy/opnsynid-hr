c00de33dbd4b996be57419bb6aef694dcd77183e
mikevhe18 <viriyananda.michael@gmail.com>
2018-09-04 18:21:24 +0700

    [8.0.1.0.0][hr_attendance_overtime_request_tier_validation]

---
+++ hr_attendance_overtime_request_tier_validation/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Attendance Overtime Request Tier Validation",
+    "version": "8.0.1.0.0",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "hr_attendance_overtime_request",
+        "base_tier_validation",
+    ],
+    "data": [
+        "views/hr_overtime_request_views.xml"
+    ],
+}
