fe15a507b05b2c6954e32db8c03fcd5ac26d3e34
Andhitia Rama <andhitia.r@gmail.com>
2018-09-07 19:09:05 +0700

    [LINT] hr_holiday_code

--- hr_holiday_code/__openerp__.py
+++ hr_holiday_code/__openerp__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # Copyright 2018 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
 {
     "name": "HR Holiday Code",
     "version": "8.0.1.0.0",
