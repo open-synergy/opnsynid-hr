4dc11cad598fbd065b63660fc8c1686f36a60908
mikevhe18 <viriyananda.michael@gmail.com>
2018-09-24 17:53:12 +0700

    [8.0.1.2.0][hr_attendance_import_from_machine]

--- hr_attendance_import_from_machine/__openerp__.py
+++ hr_attendance_import_from_machine/__openerp__.py
@@ -4,7 +4,7 @@
 # pylint: disable=locally-disabled, manifest-required-author
 {
     "name": "Import Attendance Data From Machine",
-    "version": "8.0.1.1.0",
+    "version": "8.0.1.2.0",
     "website": "https://opensynergy-indonesia.com",
     "author": "OpenSynergy Indonesia",
     "license": "AGPL-3",
