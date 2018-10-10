92b57f367471b04211cb3df5be6a7c892874d76b
mikevhe18 <viriyananda.michael@gmail.com>
2018-10-10 17:51:56 +0700

    [8.0.1.0.0][hr_attendance_overtime_request_batch]

---
+++ hr_attendance_overtime_request_batch/models/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from . import hr_overtime_request_batch
+from . import hr_overtime_request
+from . import res_company
+from . import res_config
---
+++ hr_attendance_overtime_request_batch/models/hr_overtime_request.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields
+
+
+class HrOvertimeRequest(models.Model):
+    _inherit = "hr.overtime_request"
+
+    request_batch_id = fields.Many2one(
+        string="Request Batch",
+        comodel_name="hr.overtime_request_batch",
+        readonly=True,
+        states={
+            'draft': [('readonly', False)]
+        },
+        copy=False,
+        ondelete='restrict'
+    )
