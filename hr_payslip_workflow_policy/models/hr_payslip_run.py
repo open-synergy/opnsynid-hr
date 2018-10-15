a71bb63b5818143029f9b4aad1c35a03b685f01c
mikevhe18 <viriyananda.michael@gmail.com>
2018-10-15 10:28:04 +0700

    [8.0.1.0.0][hr_payslip_workflow_policy]

---
+++ hr_payslip_workflow_policy/models/hr_payslip_run.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields, api, SUPERUSER_ID
+
+
+class HrPayslipRun(models.Model):
+    _inherit = "hr.payslip.run"
+
+    @api.model
+    def _default_company_id(self):
+        return self.env.user.company_id.id
+
+    company_id = fields.Many2one(
+        string="Company",
+        comodel_name="res.company",
+        default=lambda self: self._default_company_id(),
+        readonly=False,
+    )
+
+    @api.multi
+    @api.depends(
+        "state",
+        "company_id",
+    )
+    def _compute_policy(self):
+        for batch in self:
+            if self.env.user.id == SUPERUSER_ID:
+                batch.close_ok = batch.generate_ok = \
+                    batch.set2draft_ok = True
+                continue
+
+            if batch.company_id:
+                company = batch.company_id
+                for policy in company.\
+                        _get_payslip_run_button_policy_map():
+                    setattr(
+                        batch,
+                        policy[0],
+                        company.
+                        _get_payslip_button_policy(
+                            policy[1]),
+                    )
+
+    close_ok = fields.Boolean(
+        string="Can Close",
+        compute="_compute_policy",
+        store=False,
+        readonly=True,
+    )
+    generate_ok = fields.Boolean(
+        string="Can Generate",
+        compute="_compute_policy",
+        store=False,
+        readonly=True,
+    )
+    set2draft_ok = fields.Boolean(
+        string="Can Set To Draft",
+        compute="_compute_policy",
+        store=False,
+        readonly=True,
+    )
---
+++ hr_payslip_workflow_policy/models/res_company.py
@@ -0,0 +1,110 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields, api
+
+
+class ResCompany(models.Model):
+    _inherit = "res.company"
+
+    payslip_confirm_grp_ids = fields.Many2many(
+        string="Allowed To Confirm",
+        comodel_name="res.groups",
+        relation="rel_payslip_allowed_confirm_groups",
+        column1="company_id",
+        column2="group_id",
+    )
+    payslip_refund_grp_ids = fields.Many2many(
+        string="Allowed To Refund",
+        comodel_name="res.groups",
+        relation="rel_payslip_allowed_refund_groups",
+        column1="company_id",
+        column2="group_id",
+    )
+    payslip_compute_grp_ids = fields.Many2many(
+        string="Allowed To Compute Sheet",
+        comodel_name="res.groups",
+        relation="rel_payslip_allowed_compute_groups",
+        column1="company_id",
+        column2="group_id",
+    )
+    payslip_set2draft_grp_ids = fields.Many2many(
+        string="Allowed To Set To Draft",
+        comodel_name="res.groups",
+        relation="rel_payslip_allowed_set2draft_groups",
+        column1="company_id",
+        column2="group_id",
+    )
+    payslip_cancel_grp_ids = fields.Many2many(
+        string="Allowed To Cancel",
+        comodel_name="res.groups",
+        relation="rel_payslip_allowed_cancel_groups",
+        column1="company_id",
+        column2="group_id",
+    )
+    payslip_run_close_grp_ids = fields.Many2many(
+        string="Allowed To Close",
+        comodel_name="res.groups",
+        relation="rel_payslip_run_allowed_close_groups",
+        column1="company_id",
+        column2="group_id",
+    )
+    payslip_run_generate_grp_ids = fields.Many2many(
+        string="Allowed To Generate Payslips",
+        comodel_name="res.groups",
+        relation="rel_payslip_run_allowed_generate_groups",
+        column1="company_id",
+        column2="group_id",
+    )
+    payslip_run_set2draft_grp_ids = fields.Many2many(
+        string="Allowed To Compute Sheet",
+        comodel_name="res.groups",
+        relation="rel_payslip_run_allowed_set2draft_groups",
+        column1="company_id",
+        column2="group_id",
+    )
+
+    @api.model
+    def _get_payslip_button_policy_map(self):
+        return [
+            ("confirm_ok",
+                "payslip_confirm_grp_ids"),
+            ("refund_ok",
+                "payslip_refund_grp_ids"),
+            ("compute_ok",
+                "payslip_compute_grp_ids"),
+            ("set2draft_ok",
+                "payslip_set2draft_grp_ids"),
+            ("cancel_ok",
+                "payslip_cancel_grp_ids"),
+        ]
+
+    @api.model
+    def _get_payslip_run_button_policy_map(self):
+        return [
+            ("close_ok",
+                "payslip_run_close_grp_ids"),
+            ("generate_ok",
+                "payslip_run_generate_grp_ids"),
+            ("set2draft_ok",
+                "payslip_run_set2draft_grp_ids"),
+        ]
+
+    @api.multi
+    def _get_payslip_button_policy(self, policy_field):
+        self.ensure_one()
+        result = False
+        button_group_ids = []
+        user = self.env.user
+        group_ids = user.groups_id.ids
+
+        button_group_ids += getattr(
+            self, policy_field).ids
+
+        if not button_group_ids:
+            result = True
+        else:
+            if (set(button_group_ids) & set(group_ids)):
+                result = True
+        return result
