c3e2283f056f2d168d891194668d71e3d2a18356
mikevhe18 <viriyananda.michael@gmail.com>
2018-10-15 10:27:51 +0700

    [8.0.1.0.0][hr_timesheet_workflow_policy]

---
+++ hr_timesheet_workflow_policy/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "HR Timesheet - Workflow Policy",
+    "version": "8.0.1.0.0",
+    "category": "Human Resource",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "hr_attendance_configuration_page",
+        "hr_timesheet_sheet",
+    ],
+    "data": [
+        "views/hr_attendance_config_setting_views.xml",
+        "views/hr_timesheet_sheet_views.xml"
+    ],
+}
