b28281098e5f8f79aab8c161d1464e81ce520453
mikevhe18 <viriyananda.michael@gmail.com>
2018-10-16 22:01:04 +0700

    [8.0.1.0.0][hr_timesheet_tier_validation]

---
+++ hr_timesheet_tier_validation/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Timesheet Tier Validation",
+    "version": "8.0.1.0.0",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "hr_timesheet_sheet",
+        "base_tier_validation",
+    ],
+    "data": [
+        "views/hr_timesheet_sheet_views.xml"
+    ],
+}
