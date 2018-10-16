89064ef572b799e0f9b358cdb2625cca8f78f698
mikevhe18 <viriyananda.michael@gmail.com>
2018-10-16 22:00:50 +0700

    [8.0.1.0.0][hr_payslip_tier_validation]

---
+++ hr_payslip_tier_validation/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Payslip Tier Validation",
+    "version": "8.0.1.0.0",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "hr_payslip_extend",
+        "base_tier_validation",
+    ],
+    "data": [
+        "views/hr_payslip_views.xml"
+    ],
+}
