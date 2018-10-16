826f88a246ac88b34eff3f250fc7c25f57ec6d3a
mikevhe18 <viriyananda.michael@gmail.com>
2018-10-16 22:00:06 +0700

    [8.0.1.0.0][hr_payslip_extend]

---
+++ hr_payslip_extend/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Payslip Extend",
+    "version": "8.0.1.0.0",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "hr_payroll"
+    ],
+    "data": [
+        "views/hr_payslip_views.xml",
+        "workflows/hr_payroll_workflow.xml"
+    ],
+}
