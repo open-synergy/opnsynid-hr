1d47d7755311ce9d281c334780cbc25c7279c711
Andhitia Rama <andhitia.r@gmail.com>
2018-12-10 06:02:50 +0700

    [8.0.1.0.0] hr_career_administration_configuration_page

---
+++ hr_career_administration_configuration_page/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Career Administration Configuration Page",
+    "version": "8.0.1.0.0",
+    "category": "Hidden",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "auto_install": True,
+    "depends": [
+        "hr",
+    ],
+    "data": [
+        "menu.xml",
+        "views/hr_career_administration_config_setting_views.xml",
+    ],
+}
