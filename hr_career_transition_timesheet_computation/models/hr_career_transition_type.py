71a8905f8f7441c5cd16822a7aba52ab341b36ba
Andhitia Rama <andhitia.r@gmail.com>
2018-12-10 14:03:18 +0700

    [8.0.1.0.0] hr_career_transition_timesheet_computation

---
+++ hr_career_transition_timesheet_computation/models/hr_career_transition_type.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields
+
+
+class HrCareerTransitionType(models.Model):
+    _inherit = "hr.career_transition_type"
+
+    change_timesheet_computation = fields.Boolean(
+        string="Change Timesheet Computation?",
+    )
