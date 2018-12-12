1b1846461fc3e4ea032231b0332a98d3aa37f271
Andhitia Rama <andhitia.r@gmail.com>
2018-12-12 07:21:35 +0700

    [8.0.1.0.0] hr_demotion_transition_payroll_account

---
+++ hr_demotion_transition_payroll_account/models/hr_demotion_transition.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import models
+
+
+class HrDemotionTransition(models.Model):
+    _name = "hr.demotion_transition"
+    _inherit = ["hr.career_transition", "hr.demotion_transition"]
