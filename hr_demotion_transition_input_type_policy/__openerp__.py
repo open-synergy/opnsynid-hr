00eb62e0f3c2ce7d822ee591c8ab1451cae3fadd
Andhitia Rama <andhitia.r@gmail.com>
2018-12-12 07:20:13 +0700

    [8.0.1.0.0] hr_demotion_transition_input_type_policy

---
+++ hr_demotion_transition_input_type_policy/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Demotion Transition - "
+            "Integration With Payslip Input Policy",
+    "version": "8.0.1.0.0",
+    "category": "Hidden",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "auto_install": True,
+    "depends": [
+        "hr_career_transition_payslip_input_policy",
+        "hr_demotion_transition",
+    ],
+    "data": [
+    ],
+}
