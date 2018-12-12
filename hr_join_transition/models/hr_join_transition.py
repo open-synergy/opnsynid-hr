73d8ede5676c158605d3b60b52f213ad183652b0
Andhitia Rama <andhitia.r@gmail.com>
2018-12-12 19:35:14 +0700

    [8.0.1.0.0] hr_join_transition

---
+++ hr_join_transition/models/hr_join_transition.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import models, api
+
+
+class HrJoinTransition(models.Model):
+    _name = "hr.join_transition"
+    _inherit = ["hr.career_transition"]
+    _description = "Career Transition - Join"
+    _table = "hr_career_transition"
+
+    @api.model
+    def _default_type_id(self):
+        return self.env.ref(
+            "hr_join_transition."
+            "career_transition_join").id
+
+    @api.model
+    def search(self, args, offset=0, limit=None, order=None, count=False):
+        type_id = self.env.ref(
+            "hr_join_transition."
+            "career_transition_join", False) and self.env.ref(
+                "hr_join_transition."
+                "career_transition_join") or self.env["hr."
+                                                      "career_transition_"
+                                                      "type"]
+        args.append(("type_id", "=", type_id.id))
+        return super(HrJoinTransition, self).search(
+            args=args, offset=offset, limit=limit,
+            order=order, count=count)
