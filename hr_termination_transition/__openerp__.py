b8b293131c0f85e9b274df61764eb2ca922a68e4
Andhitia Rama <andhitia.r@gmail.com>
2018-12-12 19:37:27 +0700

    [8.0.1.0.0] hr_termination_transition

---
+++ hr_termination_transition/__openerp__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Career Transition - Termination",
+    "version": "8.0.1.0.1",
+    "category": "Human Resource",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "hr_career_transition",
+    ],
+    "data": [
+        "data/ir_sequence_data.xml",
+        "data/hr_career_transition_type_data.xml",
+        "data/base_sequence_configurator_data.xml",
+        "data/base_workflow_policy_data.xml",
+        "views/hr_termination_transition_views.xml",
+    ],
+}
