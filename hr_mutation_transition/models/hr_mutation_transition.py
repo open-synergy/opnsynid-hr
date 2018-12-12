9c82cbb46618531daa8b88fbe9125716a53c0ba4
Andhitia Rama <andhitia.r@gmail.com>
2018-12-12 08:00:20 +0700

    [8.0.1.0.0] hr_mutation_transition

---
+++ hr_mutation_transition/models/hr_mutation_transition.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import models, api
+
+
+class HrMutationTransition(models.Model):
+    _name = "hr.mutation_transition"
+    _inherit = ["hr.career_transition"]
+    _description = "Career Transition - Mutation"
+    _table = "hr_career_transition"
+
+    @api.model
+    def _default_type_id(self):
+        return self.env.ref(
+            "hr_mutation_transition."
+            "career_transition_mutation").id
+
+    @api.model
+    def search(self, args, offset=0, limit=None, order=None, count=False):
+        type_id = self.env.ref(
+            "hr_mutation_transition."
+            "career_transition_mutation", False) and self.env.ref(
+                "hr_mutation_transition."
+                "career_transition_mutation") or self.env["hr."
+                                                          "career_transition_"
+                                                          "type"]
+        args.append(("type_id", "=", type_id.id))
+        return super(HrMutationTransition, self).search(
+            args=args, offset=offset, limit=limit,
+            order=order, count=count)
