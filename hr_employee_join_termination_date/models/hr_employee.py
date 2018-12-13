2721ecb5d5375ad902b71401662465c422ae177d
Andhitia Rama <andhitia.r@gmail.com>
2018-12-13 12:10:03 +0700

    [8.0.1.0.0] hr_employee_join_termination_date

---
+++ hr_employee_join_termination_date/models/hr_employee.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import models, fields
+
+
+class HrEmployee(models.Model):
+    _inherit = "hr.employee"
+
+    date_join = fields.Date(
+        string="Join Date",
+    )
+    date_termination = fields.Date(
+        string="Termination Date",
+    )
