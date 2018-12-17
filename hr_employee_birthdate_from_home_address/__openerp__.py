92b16774560a77bb917f3fba68cb17808c9edfd3
Andhitia Rama <andhitia.r@gmail.com>
2018-12-17 16:01:02 +0700

    [8.0.1.0.0] hr_employee_birthdate_from_home_address

---
+++ hr_employee_birthdate_from_home_address/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Employee Birthdate From Home Address",
+    "version": "8.0.1.0.0",
+    "category": "Human Resource",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "hr",
+        "partner_contact_birthdate",
+    ],
+    "data": [
+    ],
+}
