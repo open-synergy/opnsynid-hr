5f24aceae636bfd4337c1b54120d362d09e145c9
Andhitia Rama <andhitia.r@gmail.com>
2018-12-17 21:53:51 +0700

    [8.0.1.0.0] hr_employee_data_from_contract_additional_field

---
+++ hr_employee_data_from_contract_additional_field/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Employee Data From Contract Additional Field",
+    "version": "8.0.1.0.0",
+    "category": "Human Resource",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "hr_contract_additional_field",
+        "hr_employee_data_from_contract",
+    ],
+    "data": [
+    ],
+}
---
+++ hr_employee_data_from_contract_additional_field/models/hr_employee.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import models, fields
+
+
+class HrEmployee(models.Model):
+    _inherit = "hr.employee"
+
+    department_id = fields.Many2one(
+        string="Department",
+        comodel_name="hr.department",
+        related="contract_id.contract_department_id",
+        store=True,
+        readonly=True,
+    )
+    parent_id = fields.Many2one(
+        string="Manager",
+        comodel_name="hr.employee",
+        related="contract_id.contract_department_id.manager_id",
+        store=True,
+        readonly=True,
+    )
+    company_id = fields.Many2one(
+        string="Company",
+        comodel_name="res.company",
+        related="contract_id.company_id",
+        store=True,
+        readonly=True,
+    )
