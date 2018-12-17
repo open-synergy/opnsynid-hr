b5fd49934a3636b6b33a0f960ce5735943b01bc2
Andhitia Rama <andhitia.r@gmail.com>
2018-12-17 16:14:39 +0700

    [8.0.1.0.0] hr_employee_nationality_from_home_address

---
+++ hr_employee_nationality_from_home_address/models/hr_employee.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import models, fields
+
+
+class HrEmployee(models.Model):
+    _inherit = "hr.employee"
+
+    country_id = fields.Many2one(
+        string="Nationality",
+        comodel_name="res.country",
+        related="address_home_id.nationality_id",
+        store=True,
+    )
