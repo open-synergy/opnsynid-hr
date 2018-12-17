7056bfa979954d382e279bc0051596c78c49ee0c
Andhitia Rama <andhitia.r@gmail.com>
2018-12-17 20:31:12 +0700

    [8.0.1.0.0] hr_employee_data_from_contract

---
+++ hr_employee_data_from_contract/models/hr_employee.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import models, fields, api
+
+
+class HrEmployee(models.Model):
+    _inherit = "hr.employee"
+
+    @api.multi
+    @api.depends(
+        "contract_ids",
+        "contract_ids.date_start",
+    )
+    def _compute_current_contract(self):
+        for employee in self:
+            contract = False
+            contracts = employee.contract_ids.sorted(
+                key=lambda r: r.date_start, reverse=True)
+            if len(contracts) > 0:
+                contract = contracts[0]
+            employee.contract_id = contract
+
+    contract_id = fields.Many2one(
+        string="Current Contract",
+        comodel_name="hr.contract",
+        compute="_compute_current_contract",
+        store=True,
+    )
+    job_id = fields.Many2one(
+        string="Job Title",
+        comodel_name="hr.job",
+        related="contract_id.job_id",
+        readonly=True,
+        store=True,
+    )
