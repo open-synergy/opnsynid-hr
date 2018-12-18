922ee9160646bd3713167094732ac3b658077b76
Andhitia Rama <andhitia.r@gmail.com>
2018-12-18 15:58:21 +0700

    [8.0.1.0.0] hr_employee_emergency_contact_from_home_address

---
+++ hr_employee_emergency_contact_from_home_address/models/hr_employee.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import models, fields
+
+
+class HrEmployee(models.Model):
+    _inherit = "hr.employee"
+
+    emergency_contact_ids = fields.Many2many(
+        string="Emergency Contact",
+        comodel_name="res.partner",
+        related="address_home_id.emergency_contact_ids",
+        readonly=True,
+    )
