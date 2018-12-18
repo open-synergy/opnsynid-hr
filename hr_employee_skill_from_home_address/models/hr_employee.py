eba38e9497ed4716bd947177a0d22f581e60691d
Andhitia Rama <andhitia.r@gmail.com>
2018-12-18 20:40:48 +0700

    [8.0.1.0.0] hr_employee_skill_from_home_address

---
+++ hr_employee_skill_from_home_address/models/hr_employee.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import models, fields
+
+
+class HrEmployee(models.Model):
+    _inherit = "hr.employee"
+
+    skill_ids = fields.One2many(
+        comodel_name="partner.skill",
+        related="address_home_id.skill_ids",
+        string="Skills",
+    )
