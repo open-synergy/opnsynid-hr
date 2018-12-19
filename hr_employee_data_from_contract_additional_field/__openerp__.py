e84bd9424a6f77cf5474a445e64013f0395a504a
Andhitia Rama <andhitia.r@gmail.com>
2018-12-19 18:36:42 +0700

    [8.0.1.1.0] hr_employee_data_from_contract_additional_field

--- hr_employee_data_from_contract_additional_field/__openerp__.py
+++ hr_employee_data_from_contract_additional_field/__openerp__.py
@@ -4,7 +4,7 @@
 # pylint: disable=locally-disabled, manifest-required-author
 {
     "name": "Employee Data From Contract Additional Field",
-    "version": "8.0.1.0.0",
+    "version": "8.0.1.1.0",
     "category": "Human Resource",
     "website": "https://opensynergy-indonesia.com",
     "author": "OpenSynergy Indonesia",
