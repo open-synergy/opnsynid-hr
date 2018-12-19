1a3f98e9a6d248fa82423d1d81e2a575490b1188
Andhitia Rama <andhitia.r@gmail.com>
2018-12-19 10:32:33 +0700

    [8.0.1.1.0] hr_employee_experience_from_home_address

--- hr_employee_experience_from_home_address/__openerp__.py
+++ hr_employee_experience_from_home_address/__openerp__.py
@@ -4,7 +4,7 @@
 # pylint: disable=locally-disabled, manifest-required-author
 {
     "name": "Employee Experience From Home Address",
-    "version": "8.0.1.0.0",
+    "version": "8.0.1.1.0",
     "category": "Human Resource",
     "website": "https://opensynergy-indonesia.com",
     "author": "OpenSynergy Indonesia",
