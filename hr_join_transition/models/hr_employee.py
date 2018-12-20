46337f6bbd9cbde1c8dde1f0d27610075b131936
Andhitia Rama <andhitia.r@gmail.com>
2018-12-20 22:21:37 +0700

    [8.0.1.2.0] hr_join_transition
    * Retrigger latest_career_transition_id on employee

--- hr_join_transition/models/hr_employee.py
+++ hr_join_transition/models/hr_employee.py
@@ -2,11 +2,20 @@
 # Copyright 2018 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import models, fields
+from openerp import models, fields, api
 
 
 class HrEmployee(models.Model):
     _inherit = "hr.employee"
+
+    @api.multi
+    @api.depends(
+        "join_transition_ids",
+        "join_transition_ids.state",
+    )
+    def _compute_career_transition(self):
+        _super = super(HrEmployee, self)
+        _super._compute_career_transition()
 
     join_transition_ids = fields.One2many(
         string="Join Transitions",
