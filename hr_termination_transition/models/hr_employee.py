7b1901940f176a72036af6c25a653835c79645b1
Andhitia Rama <andhitia.r@gmail.com>
2018-12-20 22:24:34 +0700

    [8.0.1.2.0] hr_termination_transition
    * Retrigger latest_career_transition_id on employee

--- hr_termination_transition/models/hr_employee.py
+++ hr_termination_transition/models/hr_employee.py
@@ -2,11 +2,20 @@
 # Copyright 2018 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import models, fields
+from openerp import models, fields, api
 
 
 class HrEmployee(models.Model):
     _inherit = "hr.employee"
+
+    @api.multi
+    @api.depends(
+        "termination_transition_ids",
+        "termination_transition_ids.state",
+    )
+    def _compute_career_transition(self):
+        _super = super(HrEmployee, self)
+        _super._compute_career_transition()
 
     termination_transition_ids = fields.One2many(
         string="Termination Transitions",
