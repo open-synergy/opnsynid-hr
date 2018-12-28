c698849e4ae8717f0ddd4e1638f73a58c4c0ab7e
Andhitia Rama <andhitia.r@gmail.com>
2018-12-29 02:15:20 +0700

    [8.0.1.0.0] hr_assignment_transition

---
+++ hr_assignment_transition/models/hr_assignment_transition.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import models, api
+
+
+class HrAssignmentTransition(models.Model):
+    _name = "hr.assignment_transition"
+    _inherit = ["hr.career_transition"]
+    _description = "Career Transition - New Assignment"
+    _table = "hr_career_transition"
+
+    @api.model
+    def _default_type_id(self):
+        return self.env.ref(
+            "hr_assignment_transition."
+            "career_transition_assignment").id
+
+    @api.model
+    def search(self, args, offset=0, limit=None, order=None, count=False):
+        ref_name = "hr_assignment_transition.career_transition_assignment"
+        type_id = self.env.ref(ref_name, False) and \
+            self.env.ref(ref_name) or \
+            self.env["hr.career_transition_type"]
+        args.append(("type_id", "=", type_id.id))
+        return super(HrAssignmentTransition, self).search(
+            args=args, offset=offset, limit=limit,
+            order=order, count=count)
