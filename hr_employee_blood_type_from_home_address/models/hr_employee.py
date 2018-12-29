66a89fdd3fd9bbac60fbbb2bb3011c8abac1de21
Andhitia Rama <andhitia.r@gmail.com>
2018-12-29 08:18:35 +0700

    [8.0.1.0.0] hr_employee_blood_type_from_home_address

---
+++ hr_employee_blood_type_from_home_address/models/hr_employee.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import models, fields
+
+
+class HrEmployee(models.Model):
+    _inherit = "hr.employee"
+
+    blood_type_aob = fields.Selection(
+        string="Blood Type (ABO)",
+        selection=[
+            ("a", "A"),
+            ("b", "B"),
+            ("ab", "AB"),
+            ("o", "O"),
+        ],
+        related="address_home_id.blood_type_aob",
+        store=True,
+    )
+    blood_type_rh = fields.Selection(
+        string="Blood Type (Rh)",
+        selection=[
+            ("+", "+"),
+            ("-", "-"),
+        ],
+        related="address_home_id.blood_type_rh",
+        store=True,
+    )
