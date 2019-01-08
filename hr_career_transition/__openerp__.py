6d926fcc1d728eefc714a21106e0410d6d1471ee
Andhitia Rama <andhitia.r@gmail.com>
2019-01-09 00:06:45 +0700

    [8.0.1.4.0] hr_career_transition
    * Revert contract on cancel
    * Change onchange mechanism on previous_contract_id

--- hr_career_transition/__openerp__.py
+++ hr_career_transition/__openerp__.py
@@ -4,7 +4,7 @@
 # pylint: disable=locally-disabled, manifest-required-author
 {
     "name": "Career Transition",
-    "version": "8.0.1.3.1",
+    "version": "8.0.1.4.0",
     "category": "Human Resource",
     "website": "https://opensynergy-indonesia.com",
     "author": "OpenSynergy Indonesia",
