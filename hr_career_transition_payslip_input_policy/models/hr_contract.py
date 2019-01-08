6d4d2464e7babd7c847e04b7e8370a3ff4c2a018
Andhitia Rama <andhitia.r@gmail.com>
2019-01-09 00:13:36 +0700

    [8.0.1.1.0] hr_career_transition_payslip_input_policy
    * Revert contract on cancel
    * Adjust onchange mechanism

---
+++ hr_career_transition_payslip_input_policy/models/hr_contract.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, api
+
+
+class HrContract(models.Model):
+    _inherit = "hr.contract"
+
+    @api.multi
+    def _get_input_types_dict(self):
+        self.ensure_one()
+        result = []
+        for input_type in self.input_type_ids:
+            result.append((0, 0, {
+                "input_type_id": input_type.input_type_id.id,
+                "amount": input_type.amount,
+            }))
+        return result
