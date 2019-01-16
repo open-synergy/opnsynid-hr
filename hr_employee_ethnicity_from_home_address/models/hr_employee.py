04830bce21121d18beb12ae940ebe94665a0b746
Andhitia Rama <andhitia.r@gmail.com>
2019-01-17 06:31:48 +0700

    [8.0.1.0.0] hr_employee_ethnicity_from_home_address

---
+++ hr_employee_ethnicity_from_home_address/models/hr_employee.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import models, fields
+
+
+class HrEmployee(models.Model):
+    _inherit = "hr.employee"
+
+    ethnicity_id = fields.Many2one(
+        string="Ethnicity",
+        comodel_name="partner.ethnicity",
+        related="address_home_id.ethnicity_id",
+        store=True,
+    )
