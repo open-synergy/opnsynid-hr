a029a3322db7c6430f740976532f4a1cf9402ab2
Andhitia Rama <andhitia.r@gmail.com>
2019-01-25 20:00:58 +0700

    [8.0.1.3.0] hr_timesheet_computation
    * Add Timesheet Computation Analysis

--- hr_timesheet_computation/reports/__init__.py
+++ hr_timesheet_computation/reports/__init__.py
@@ -2,4 +2,6 @@
 # Copyright 2018 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from . import models, reports
+from . import (
+    hr_timesheet_computation_analysis,
+)
