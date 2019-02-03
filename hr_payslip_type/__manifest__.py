3cdbdad60c7b7c98d8d8c22e02868327b5a44be6
Andhitia Rama <andhitia.r@gmail.com>
2019-02-03 10:06:26 +0700

    [12.0.1.0.0] hr_payslip_type

--- hr_payslip_type/__manifest__.py
+++ hr_payslip_type/__manifest__.py
@@ -1,9 +1,8 @@
-# -*- coding: utf-8 -*-
-# Copyright 2018 OpenSynergy Indonesia
+# Copyright 2018-2019 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Human Resource - Payslip Type",
-    "version": "10.0.1.0.0",
+    "version": "12.0.1.0.0",
     "category": "Human Resource",
     "website": "https://opensynergy-indonesia.com",
     "author": "OpenSynergy Indonesia",
--- hr_payslip_type/models/hr_payslip.py
+++ hr_payslip_type/models/hr_payslip.py
@@ -1,18 +1,18 @@
-# -*- coding: utf-8 -*-
-# Copyright 2017 OpenSynergy Indonesia
+# Copyright 2017-2019 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class HrPayslip(models.Model):
     _inherit = "hr.payslip"
 
+    @api.model
     def _default_payslip_type(self):
         return self.env['hr.payslip.type'].search([])[:1]
 
     payslip_type_id = fields.Many2one(
         string="Payslip Type",
         comodel_name="hr.payslip.type",
-        default=_default_payslip_type
+        default=lambda self: self._default_payslip_type(),
     )
