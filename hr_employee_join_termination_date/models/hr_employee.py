20d1fbeb3cc72f16b794f5602dfc36320c17bd9f
Andhitia Rama <andhitia.r@gmail.com>
2019-02-03 21:42:53 +0700

    [12.0.1.0.0] hr_employee_join_termination_date

--- hr_employee_join_termination_date/models/hr_employee.py
+++ hr_employee_join_termination_date/models/hr_employee.py
@@ -1,8 +1,7 @@
-# -*- coding: utf-8 -*-
-# Copyright 2018 OpenSynergy Indonesia
+# Copyright 2018-2019 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, fields
+from odoo import models, fields
 
 
 class HrEmployee(models.Model):
