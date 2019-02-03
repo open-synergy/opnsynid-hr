bd648d7d607c6b40498464ecb7dec5fc0a7a13a9
Andhitia Rama <andhitia.r@gmail.com>
2019-02-03 09:30:35 +0700

    [12.0.1.0.0] hr_department_type

--- hr_department_type/models/hr_department.py
+++ hr_department_type/models/hr_department.py
@@ -1,5 +1,4 @@
-# -*- coding: utf-8 -*-
-# Copyright 2018 OpenSynergy Indonesia
+# Copyright 2018-2019 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import models, fields
