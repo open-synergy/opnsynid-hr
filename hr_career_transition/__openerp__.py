d878fce1c002e0b7b9062064da0a2ca0b48c440f
Andhitia Rama <andhitia.r@gmail.com>
2019-02-08 10:44:45 +0700

    [8.0.1.7.0] hr_career_transition
    * Fix group by company

--- hr_career_transition/__openerp__.py
+++ hr_career_transition/__openerp__.py
@@ -4,7 +4,7 @@
 # pylint: disable=locally-disabled, manifest-required-author
 {
     "name": "Career Transition",
-    "version": "8.0.1.6.0",
+    "version": "8.0.1.7.0",
     "category": "Human Resource",
     "website": "https://opensynergy-indonesia.com",
     "author": "OpenSynergy Indonesia",
