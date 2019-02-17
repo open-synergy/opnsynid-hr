f549be5e168cd50363afc54f9494d149b834ad1d
Andhitia Rama <andhitia.r@gmail.com>
2019-02-18 00:17:23 +0700

    [8.0.1.0.0] hr_expense_security

---
+++ hr_expense_security/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Expense Security",
+    "version": "8.0.1.0.0",
+    "category": "Human Resource",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "hr_expense",
+    ],
+    "data": [
+        "security/ir_module_category_data.xml",
+        "security/res_groups_data.xml",
+        "security/ir_rule_data.xml",
+        "views/hr_expense_views.xml",
+    ],
+}
