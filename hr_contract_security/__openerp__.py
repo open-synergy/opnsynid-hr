159ae6a1a1632dde6d3b6fc41af1234fda4eac3e
Andhitia Rama <andhitia.r@gmail.com>
2019-02-18 22:48:08 +0700

    [8.0.3.0.0] hr_contract_security
    * Change dependency into hr_contract_additional_field
    * Employee can only read

--- hr_contract_security/__openerp__.py
+++ hr_contract_security/__openerp__.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
-# Copyright 2018 OpenSynergy Indonesia
+# Copyright 2018-2019 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 # pylint: disable=locally-disabled, manifest-required-author
 {
     "name": "Employee Contract Security",
-    "version": "8.0.2.0.0",
+    "version": "8.0.3.0.0",
     "category": "Human Resources",
     "website": "https://opensynergy-indonesia.com",
     "author": "OpenSynergy Indonesia",
     "license": "AGPL-3",
     "installable": True,
     "depends": [
-        "hr_contract",
+        "hr_contract_additional_field",
     ],
     "data": [
         "security/ir_module_category_data.xml",
