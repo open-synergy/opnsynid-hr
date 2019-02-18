a8c9bab49487259af80380a548810f01c95dd0d8
Andhitia Rama <andhitia.r@gmail.com>
2019-02-18 21:49:04 +0700

    [8.0.1.3.0] hr_contract_additional_field
    [hr_contract_additional_field] onchange method on hr.contract
    
    [hr_contract_additional_field] Remove on_change attribute from hr.contract form
    
    [hr_contract_additional_field] Add tree view extension for hr.contract
    
    [hr_contract_additional_field] Add search view extension for hr.contract
    
    [hr_contract_additional_field] Add translation file
    
    [hr_contract_additional_field] default method for employee_id

--- hr_contract_additional_field/__openerp__.py
+++ hr_contract_additional_field/__openerp__.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
-# Copyright 2018 OpenSynergy Indonesia
+# Copyright 2018-2019 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 # pylint: disable=locally-disabled, manifest-required-author
 {
     "name": "Additional Field for Employee Contract",
-    "version": "8.0.1.2.0",
+    "version": "8.0.1.3.0",
     "category": "Human Resource",
     "website": "https://opensynergy-indonesia.com",
     "author": "OpenSynergy Indonesia",
--- hr_contract_additional_field/models/hr_contract.py
+++ hr_contract_additional_field/models/hr_contract.py
@@ -1,12 +1,17 @@
 # -*- coding: utf-8 -*-
-# Copyright 2018 OpenSynergy Indonesia
+# Copyright 2018-2019 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import models, fields
+from openerp import models, fields, api
 
 
 class HrContract(models.Model):
     _inherit = "hr.contract"
+
+    @api.model
+    def _default_employee_id(self):
+        if len(self.env.user.employee_ids) > 0:
+            return self.env.user.employee_ids[0].id
 
     contract_department_id = fields.Many2one(
         string="Department",
@@ -17,3 +22,24 @@ class HrContract(models.Model):
         string="Company",
         comodel_name="res.company",
     )
+    employee_id = fields.Many2one(
+        default=lambda self: self._default_employee_id(),
+    )
+
+    @api.onchange("employee_id")
+    def onchange_contract_department_id(self):
+        self.contract_department_id = False
+        if self.employee_id:
+            self.contract_department_id = self.employee_id.department_id
+
+    @api.onchange("employee_id")
+    def onchange_company_id(self):
+        self.company_id = False
+        if self.employee_id:
+            self.company_id = self.employee_id.company_id
+
+    @api.onchange("employee_id")
+    def onchange_job_id(self):
+        self.job_id = False
+        if self.employee_id:
+            self.job_id = self.employee_id.job_id
