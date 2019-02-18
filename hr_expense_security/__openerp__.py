2d80b79bbfa1ec20e77836f4c6f6fad67191247f
Andhitia Rama <andhitia.r@gmail.com>
2019-02-18 08:03:58 +0700

    [8.0.1.1.0] hr_expense_security
    * Employee can only read

--- hr_expense_security/__openerp__.py
+++ hr_expense_security/__openerp__.py
@@ -4,7 +4,7 @@
 # pylint: disable=locally-disabled, manifest-required-author
 {
     "name": "Expense Security",
-    "version": "8.0.1.0.0",
+    "version": "8.0.1.1.0",
     "category": "Human Resource",
     "website": "https://opensynergy-indonesia.com",
     "author": "OpenSynergy Indonesia",
@@ -16,7 +16,9 @@
     "data": [
         "security/ir_module_category_data.xml",
         "security/res_groups_data.xml",
+        "security/ir.model.access.csv",
         "security/ir_rule_data.xml",
+        "security/ir_model_access_data.xml",
         "views/hr_expense_views.xml",
     ],
 }
