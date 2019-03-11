6903f0f5eda62aa917713f397e78f5a487b288be
Andhitia Rama <andhitia.r@gmail.com>
2019-03-11 18:16:52 +0700

    [8.0.1.0.0] hr_dicipline

---
+++ hr_dicipline/__openerp__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Employee Dicipline",
+    "version": "8.0.1.0.0",
+    "category": "Human Resource",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "hr_career_administration_configuration_page",
+        "base_sequence_configurator",
+        "base_workflow_policy",
+    ],
+    "data": [
+        "security/ir_module_category_data.xml",
+        "security/res_groups_data.xml",
+        "security/ir.model.access.csv",
+        "security/ir_rule_data.xml",
+        "data/base_workflow_policy_data.xml",
+        "data/ir_sequence_data.xml",
+        "data/base_sequence_configurator_data.xml",
+        "menu.xml",
+        "views/hr_dicipline_type_views.xml",
+        "views/hr_dicipline_reason_views.xml",
+        "views/hr_dicipline_views.xml",
+    ],
+}
---
+++ hr_dicipline/models/hr_dicipline_reason.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields
+
+
+class HrDiciplineReason(models.Model):
+    _name = "hr.dicipline_reason"
+    _description = "Dicipline Reason"
+
+    name = fields.Char(
+        string="Dicipline Reason",
+        required=True,
+    )
+    dicipline_type_id = fields.Many2one(
+        string="Dicipline Type",
+        comodel_name="hr.dicipline_type",
+        required=True,
+    )
+    code = fields.Char(
+        string="Code",
+    )
+    active = fields.Boolean(
+        string="Active",
+        default=True,
+    )
+    note = fields.Text(
+        string="Note"
+    )
