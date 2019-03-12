5daa7ab792255df8d2b8f251f3cf1e2d6491b0c7
Andhitia Rama <andhitia.r@gmail.com>
2019-03-12 18:05:58 +0700

    [8.0.1.0.0] hr_payslip_loan

---
+++ hr_payslip_loan/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Payslip Integration With Loan",
+    "version": "8.0.1.0.0",
+    "category": "Human Resource",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "hr_payroll",
+        "loan_core",
+    ],
+    "data": [
+        "views/hr_payslip_views.xml",
+    ],
+}
