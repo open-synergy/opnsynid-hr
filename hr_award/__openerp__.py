cc38d7a9f44c14075f02f8441507022ff15bc244
Andhitia Rama <andhitia.r@gmail.com>
2019-03-13 17:57:02 +0700

    [8.0.1.0.0] hr_award

---
+++ hr_award/__openerp__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Employee Award",
+    "version": "8.0.1.0.0",
+    "category": "Human Resource",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "hr_career_administration_configuration_page",
+        "base_sequence_configurator",
+        "base_workflow_policy",
+        "base_cancel_reason",
+    ],
+    "data": [
+        "security/ir_module_category_data.xml",
+        "security/res_groups_data.xml",
+        "security/ir.model.access.csv",
+        "security/ir_rule_data.xml",
+        "data/base_workflow_policy_data.xml",
+        "data/ir_sequence_data.xml",
+        "data/base_sequence_configurator_data.xml",
+        "data/base_cancel_reason_config_data.xml",
+        "menu.xml",
+        "views/hr_award_type_views.xml",
+        "views/hr_award_reason_views.xml",
+        "views/hr_award_views.xml",
+    ],
+}
---
+++ hr_award/models/hr_award_reason.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields
+
+
+class HrAwardReason(models.Model):
+    _name = "hr.award_reason"
+    _description = "Award Reason"
+
+    name = fields.Char(
+        string="Award Reason",
+        required=True,
+    )
+    award_type_id = fields.Many2one(
+        string="Award Type",
+        comodel_name="hr.award_type",
+        required=True,
+    )
+    code = fields.Char(
+        string="Code",
+    )
+    active = fields.Boolean(
+        string="Active",
+        default=True,
+    )
+    note = fields.Text(
+        string="Note"
+    )
