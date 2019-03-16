efa353e27584e4db4b938e45fff7506560a6850d
Andhitia Rama <andhitia.r@gmail.com>
2019-03-16 07:24:09 +0700

    [8.0.1.0.0] hr_employee_job_family_from_contract

---
+++ hr_employee_job_family_from_contract/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Employee Job Family From Contract",
+    "version": "8.0.1.0.0",
+    "category": "Human Resource",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "hr_employee_data_from_contract",
+        "hr_job_family_modelling",
+    ],
+    "data": [
+        "views/hr_contract_views.xml",
+    ],
+}
