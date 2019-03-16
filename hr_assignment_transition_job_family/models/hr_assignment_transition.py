ce18c10fc39479e6e9651d3eb3dd6a213ae5d439
Andhitia Rama <andhitia.r@gmail.com>
2019-03-16 21:43:43 +0700

    [8.0.1.0.0] hr_assignment_transition_job_family

---
+++ hr_assignment_transition_job_family/models/hr_assignment_transition.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import models
+
+
+class HrAssignmentTransition(models.Model):
+    _name = "hr.assignment_transition"
+    _inherit = ["hr.career_transition", "hr.assignment_transition"]
