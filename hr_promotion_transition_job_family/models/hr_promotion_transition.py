eff39a18221fca9dde98b5cb0eae047945672616
Andhitia Rama <andhitia.r@gmail.com>
2019-03-16 21:40:40 +0700

    [8.0.1.0.0] hr_promotion_transition_job_family

---
+++ hr_promotion_transition_job_family/models/hr_promotion_transition.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import models
+
+
+class HrPromotionTransition(models.Model):
+    _name = "hr.promotion_transition"
+    _inherit = ["hr.career_transition", "hr.promotion_transition"]
