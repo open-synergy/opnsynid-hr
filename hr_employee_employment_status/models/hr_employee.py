122bd27f55079dce66e21fd015110239078e4423
Andhitia Rama <andhitia.r@gmail.com>
2019-03-19 21:03:16 +0700

    [8.0.1.0.0] hr_employee_employment_status

---
+++ hr_employee_employment_status/models/hr_employee.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import models, fields
+
+
+class HrEmployee(models.Model):
+    _inherit = "hr.employee"
+
+    employment_status_id = fields.Many2one(
+        string="Employment Status",
+        comodel_name="hr.employment_status",
+    )
