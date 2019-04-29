bb1be2c43e72bd853a9b64fb416a40ea5a3af4fd
Andhitia Rama <andhitia.r@gmail.com>
2019-04-29 21:04:34 +0700

    [8.0.1.0.0] hr_payslip_gamification

---
+++ hr_payslip_gamification/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Payslip Integration With Gamification",
+    "version": "8.0.1.0.0",
+    "category": "Human Resource",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "hr_payroll",
+        "gamification",
+    ],
+    "data": [
+        "views/hr_payslip_views.xml",
+    ],
+}
