40b9b62880717aa72887d267b61dbffbf4e6882f
Andhitia Rama <andhitia.r@gmail.com>
2019-05-01 13:19:01 +0700

    [8.0.1.0.1] hr_payslip_timesheet_computation
    * Remove unstable code

--- hr_payslip_timesheet_computation/__openerp__.py
+++ hr_payslip_timesheet_computation/__openerp__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
-# Copyright 2018 OpenSynergy Indonesia
+# Copyright 2018-2019 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Payslip Integration With Timesheet Computation",
-    "version": "8.0.1.0.0",
+    "version": "8.0.1.0.1",
     "category": "Human Resource",
     "website": "https://opensynergy-indonesia.com",
     "author": "OpenSynergy Indonesia",
