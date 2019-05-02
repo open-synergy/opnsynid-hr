3f28d2c66a8fb578303abd427487f3d7966c21e5
mikevhe18 <viriyananda.michael@gmail.com>
2019-05-02 08:42:13 +0700

    [8.0.1.0.0][hr_promotion_transition_timesheet_computation]

---
+++ hr_promotion_transition_timesheet_computation/models/hr_promotion_transition.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import models
+
+
+class HrPromotionTransition(models.Model):
+    _name = "hr.promotion_transition"
+    _inherit = [
+        "hr.promotion_transition",
+        "hr.career_transition"
+    ]
