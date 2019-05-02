38a183fe6b5882f6329110fa9958248da3042ab0
mikevhe18 <viriyananda.michael@gmail.com>
2019-05-02 08:42:25 +0700

    [8.0.1.0.0][hr_termination_transition_timesheet_computation]

---
+++ hr_termination_transition_timesheet_computation/models/hr_termination_transition.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import models
+
+
+class HrTerminationTransition(models.Model):
+    _name = "hr.termination_transition"
+    _inherit = [
+        "hr.termination_transition",
+        "hr.career_transition"
+    ]
