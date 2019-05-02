f59c7502f08edf95a98147773aa05f1ed1b81544
mikevhe18 <viriyananda.michael@gmail.com>
2019-05-02 08:40:22 +0700

    [8.0.1.0.0][hr_demotion_transition_timesheet_computation]

---
+++ hr_demotion_transition_timesheet_computation/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Demotion Career Transition "
+            "Integration With Timesheet Computation",
+    "version": "8.0.1.0.0",
+    "category": "Human Resource",
+    "website": "https://opensynergy-indonesia.com/",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "auto_install": True,
+    "depends": [
+        "hr_demotion_transition",
+        "hr_career_transition_timesheet_computation"
+    ],
+    "data": [
+    ],
+}
---
+++ hr_demotion_transition_timesheet_computation/models/hr_demotion_transition.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import models
+
+
+class HrDemotionTransition(models.Model):
+    _name = "hr.demotion_transition"
+    _inherit = [
+        "hr.demotion_transition",
+        "hr.career_transition"
+    ]
