789e1119cecce0b5d17c15f80e59928475c71e51
Andhitia Rama <andhitia.r@gmail.com>
2019-05-17 10:52:12 +0700

    [8.0.1.0.0] gamification_manual_goal_creation

---
+++ gamification_manual_goal_creation/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Manually Create Gamification Goal",
+    "version": "8.0.1.0.0",
+    "category": "Human Resources",
+    "website": "https://opensynergy-indonesia.com/",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "gamification",
+    ],
+    "data": [
+        "wizards/manual_goal_creation.xml",
+        "views/gamification_challange_views.xml",
+    ],
+}
