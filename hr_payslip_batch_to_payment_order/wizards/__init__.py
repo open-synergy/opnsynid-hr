cda26e31d3998029070d71551dd12fa6eed3c646
Andhitia Rama <andhitia.r@gmail.com>
2019-05-24 15:09:35 +0700

    Test

--- hr_payslip_batch_to_payment_order/wizards/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright 2019 OpenSynergy Indonesia
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
-from . import (
-    create_payment_order_from_payslip_batch,
-)
