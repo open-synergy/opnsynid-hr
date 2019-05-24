739e7da5312f87bcdeb7acf3623101cdb666abb0
Andhitia Rama <andhitia.r@gmail.com>
2019-05-24 11:23:56 +0700

    [8.0.1.0.0] hr_payslip_batch_to_payment_order

---
+++ hr_payslip_batch_to_payment_order/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Payslip Batch To Payment Order",
+    "version": "8.0.1.0.0",
+    "category": "Human Resource",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "hr_payroll_account",
+        "account_payment",
+    ],
+    "data": [
+        "wizards/create_payment_order_from_payslip_batch.xml",
+        "views/hr_payslip_run_views.xml",
+    ],
+}
