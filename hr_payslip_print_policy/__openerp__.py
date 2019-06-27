537ed140dfc5cc7177cc208fd7fc4af632a93152
mikevhe18 <viriyananda.michael@gmail.com>
2019-06-27 12:38:06 +0700

    [8.0.1.0.0][hr_payslip_print_policy]

---
+++ hr_payslip_print_policy/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "HR Payslip - Print Policy",
+    "version": "8.0.1.0.0",
+    "category": "Human Resource",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "hr_payroll",
+        "base_print_policy"
+    ],
+    "data": [
+        "views/hr_payslip_views.xml",
+        "views/hr_payslip_run_views.xml"
+    ],
+}
