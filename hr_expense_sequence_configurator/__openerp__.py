bac323d83c97f78091a1eb4c7c793f1b43645027
Andhitia Rama <andhitia.r@gmail.com>
2020-04-03 20:04:52 +0700

    [8.0.1.0.0] hr_expense_sequence_configurator

---
+++ hr_expense_sequence_configurator/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Expense Sequence Configurator",
+    "version": "8.0.1.0.1",
+    "category": "Human Resource",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "hr_expense",
+        "base_sequence_configurator",
+    ],
+    "data": [
+        "data/ir_sequence_data.xml",
+        "data/base_sequence_configurator_data.xml",
+    ],
+}
