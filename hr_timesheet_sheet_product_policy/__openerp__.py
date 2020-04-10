95345f90b9c9df574953bf5571d38239070f57a1
Andhitia Rama <andhitia.r@gmail.com>
2020-04-11 06:37:34 +0700

    [8.0.1.0.0] hr_timesheet_sheet_product_policy/

---
+++ hr_timesheet_sheet_product_policy/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 OpenSynergy Indonesia
+# Copyright 2020 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Timesheet Tracking Product Policy",
+    "version": "8.0.1.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "hr_timesheet_sheet",
+        "hr_timesheet_product_policy",
+    ],
+    "auto_install": True,
+    "data": [
+        "views/hr_timesheet_sheet_sheet_views.xml",
+    ],
+}
