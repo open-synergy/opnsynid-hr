3fb2f1704ba83da9d9c647ccb31b13ee61af7199
Andhitia Rama <andhitia.r@gmail.com>
2020-04-11 06:08:42 +0700

    [8.0.1.0.0] hr_timesheet_product_policy

---
+++ hr_timesheet_product_policy/models/hr_analytic_timesheet.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 OpenSynergy Indonesia
+# Copyright 2020 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields
+
+
+class HrAnalyticTimesheet(models.Model):
+    _name = "hr.analytic.timesheet"
+    _inherit = "hr.analytic.timesheet"
+
+    timesheet_account_allowed_product_categ_ids = fields.Many2many(
+        string="Allowed Product Categories",
+        comodel_name="product.category",
+        related="account_id.timesheet_account_allowed_product_categ_ids",
+        readonly=True,
+    )
+    timesheet_account_allowed_product_ids = fields.Many2many(
+        string="Allowed Product",
+        comodel_name="product.product",
+        related="account_id.timesheet_account_allowed_product_ids",
+        readonly=True,
+    )
