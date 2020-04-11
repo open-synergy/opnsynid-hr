096fbb29346e606013dddec2dc3fd2239b41b58f
Andhitia Rama <andhitia.r@gmail.com>
2020-04-11 15:39:14 +0700

    [8.0.1.1.0] hr_timesheet_sheet_product_policy

--- hr_timesheet_sheet_product_policy/__openerp__.py
+++ hr_timesheet_sheet_product_policy/__openerp__.py
@@ -4,7 +4,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Timesheet Tracking Product Policy",
-    "version": "8.0.1.0.0",
+    "version": "8.0.1.1.0",
     "website": "https://simetri-sinergi.id",
     "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
     "license": "AGPL-3",
