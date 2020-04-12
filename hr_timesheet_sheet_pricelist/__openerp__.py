f3ad810f6e8414abb859ddf1519f8e4e016d8884
Andhitia Rama <andhitia.r@gmail.com>
2020-04-12 09:59:22 +0700

    [8.0.1.0.0] hr_timesheet_sheet_pricelist

---
+++ hr_timesheet_sheet_pricelist/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 OpenSynergy Indonesia
+# Copyright 2020 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Timesheet Pricelist",
+    "version": "8.0.1.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "auto_install": True,
+    "depends": [
+        "hr_timesheet_sheet",
+        "hr_timesheet_pricelist",
+    ],
+    "data": [
+        "views/hr_timesheet_sheet_sheet_views.xml",
+    ],
+}
