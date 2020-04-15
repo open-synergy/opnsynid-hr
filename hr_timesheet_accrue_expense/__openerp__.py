a393c34a217852b418c2952c942605a009efb5dd
Andhitia Rama <andhitia.r@gmail.com>
2020-04-15 19:24:39 +0700

    [8.0.1.0.1] hr_timesheet_accrue_expense

--- hr_timesheet_accrue_expense/__openerp__.py
+++ hr_timesheet_accrue_expense/__openerp__.py
@@ -4,7 +4,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Generate Accrue Expense Based On Timesheet Details",
-    "version": "8.0.1.0.0",
+    "version": "8.0.1.0.1",
     "website": "https://simetri-sinergi.id",
     "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
     "license": "AGPL-3",
