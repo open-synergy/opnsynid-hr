d99378693b92eb6ed67a4eb9cbe869f12b007d61
Andhitia Rama <andhitia.r@gmail.com>
2020-04-18 21:37:39 +0700

    [8.0.1.0.0] hr_timesheet_task_pricelist

---
+++ hr_timesheet_task_pricelist/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 OpenSynergy Indonesia
+# Copyright 2020 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Task Timesheet Activities Pricelist",
+    "version": "8.0.1.1.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "auto_install": True,
+    "depends": [
+        "hr_timesheet_task_onchange",
+        "hr_timesheet_pricelist",
+    ],
+    "data": [
+        "views/project_task_views.xml",
+    ],
+}
