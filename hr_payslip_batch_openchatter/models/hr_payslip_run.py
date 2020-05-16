fc0cdefd749b59bde9e0f2adf42e2e9a2360546f
mikevhe18 <viriyananda.michael@gmail.com>
2020-05-16 08:23:57 +0700

    [8.0.1.0.0][hr_payslip_batch_openchatter]

---
+++ hr_payslip_batch_openchatter/models/hr_payslip_run.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 OpenSynergy Indonesia
+# Copyright 2020 PT Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models
+
+
+class HRPayslipRun(models.Model):
+    _name = 'hr.payslip.run'
+    _inherit = [
+        'hr.payslip.run',
+        'mail.thread'
+    ]
