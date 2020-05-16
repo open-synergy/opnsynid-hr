4ef62637ec02eb00e3ae94a2114f5ae4746d923f
mikevhe18 <viriyananda.michael@gmail.com>
2020-05-16 08:23:57 +0700

    [8.0.1.0.0][hr_payslip_batch_openchatter]

---
+++ hr_payslip_batch_openchatter/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 OpenSynergy Indonesia
+# Copyright 2020 PT Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Open Chatter Pada Pay Slip Batch",
+    "version": "8.0.1.0.0",
+    "category": "Human Resource",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "hr_payroll",
+    ],
+    "data": [
+        "views/hr_payslip_run_views.xml",
+    ],
+}
