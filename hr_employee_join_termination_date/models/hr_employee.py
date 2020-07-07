79bcabab56768398881dde7fb8a7924ece8e48ad
mikevhe18 <viriyananda.michael@gmail.com>
2020-07-07 12:23:45 +0700

    [8.0.1.2.0][hr_employee_join_termination_date]
    [8.0.1.2.0][hr_employee_join_termination_date] Revision Author

--- hr_employee_join_termination_date/models/hr_employee.py
+++ hr_employee_join_termination_date/models/hr_employee.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # Copyright 2018 OpenSynergy Indonesia
+# Copyright 2020 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from openerp import api, models, fields
@@ -15,32 +16,6 @@ except (ImportError, IOError) as err:
 
 class HrEmployee(models.Model):
     _inherit = "hr.employee"
-
-    @api.multi
-    @api.depends(
-        "date_join",
-        "date_termination",
-    )
-    def _compute_work_longetivity(self):
-        for document in self:
-            year_work = month_work = 0
-            if document.date_join and document.date_termination:
-                dt_join =\
-                    pd.to_datetime(document.date_join)
-                dt_termination =\
-                    pd.to_datetime(document.date_termination)
-
-                dt_year_work = (dt_termination - dt_join)
-                year_work =\
-                    int(dt_year_work / np.timedelta64(1, "Y"))
-                dt_temp_year = dt_join + pd.DateOffset(years=year_work)
-
-                dt_month_work = (dt_termination - dt_temp_year)
-                month_work =\
-                    int(dt_month_work / np.timedelta64(1, "M"))
-
-            document.year_work_longetivity = year_work
-            document.month_work_longetivity = month_work
 
     date_join = fields.Date(
         string="Join Date",
@@ -58,3 +33,39 @@ class HrEmployee(models.Model):
         compute="_compute_work_longetivity",
         store=True,
     )
+
+    @api.multi
+    @api.depends(
+        "date_join",
+        "date_termination",
+    )
+    def _compute_work_longetivity(self):
+        for document in self:
+            year_work = month_work = 0
+            if document.date_join:
+                if not document.date_termination:
+                    dt_termination = \
+                        pd.to_datetime(fields.Date.today())
+                else:
+                    dt_termination =\
+                        pd.to_datetime(document.date_termination)
+                dt_join =\
+                    pd.to_datetime(document.date_join)
+
+                dt_year_work = (dt_termination - dt_join)
+                year_work =\
+                    int(dt_year_work / np.timedelta64(1, "Y"))
+                dt_temp_year = dt_join + pd.DateOffset(years=year_work)
+
+                dt_month_work = (dt_termination - dt_temp_year)
+                month_work =\
+                    int(dt_month_work / np.timedelta64(1, "M"))
+
+            document.year_work_longetivity = year_work
+            document.month_work_longetivity = month_work
+
+    @api.model
+    def cron_update_longetivity(self):
+        employee_ids = self.search([])
+        for employee in employee_ids:
+            employee._compute_work_longetivity()
