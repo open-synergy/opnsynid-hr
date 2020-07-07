92e4c89fdf2a5cc618f2d96c660773a9acbaaa4f
mikevhe18 <viriyananda.michael@gmail.com>
2020-07-07 12:23:45 +0700

    [8.0.1.2.0][hr_employee_join_termination_date]
    [8.0.1.2.0][hr_employee_join_termination_date] Revision Author

--- hr_employee_join_termination_date/__openerp__.py
+++ hr_employee_join_termination_date/__openerp__.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 # Copyright 2018 OpenSynergy Indonesia
+# Copyright 2020 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 # pylint: disable=locally-disabled, manifest-required-author
 {
     "name": "Employee Join and Termination Date",
-    "version": "8.0.1.1.0",
+    "version": "8.0.1.2.0",
     "category": "Human Resource",
-    "website": "https://opensynergy-indonesia.com",
-    "author": "OpenSynergy Indonesia",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
     "license": "AGPL-3",
     "installable": True,
     "depends": [
@@ -20,6 +21,7 @@
         ],
     },
     "data": [
+        "data/ir_cron.xml",
         "views/hr_employee_views.xml",
     ],
 }
