4f09344176266183cc3971e580ac00e490671795
mikevhe18 <viriyananda.michael@gmail.com>
2020-07-21 17:22:39 +0700

    [8.0.2.0.0][hr_demotion_transition]

--- hr_demotion_transition/models/__init__.py
+++ hr_demotion_transition/models/__init__.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 # Copyright 2018 OpenSynergy Indonesia
+# Copyright 2020 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
-from . import hr_demotion_transition
-from . import hr_employee
+from . import(
+    hr_demotion_transition,
+    hr_employee,
+    tier_definition,
+)
