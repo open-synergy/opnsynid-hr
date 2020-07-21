ac22e903e95d78410f441568df86746d74e5009a
mikevhe18 <viriyananda.michael@gmail.com>
2020-07-21 17:23:45 +0700

    [8.0.2.0.0][hr_termination_transition]

--- hr_termination_transition/models/hr_employee.py
+++ hr_termination_transition/models/hr_employee.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # Copyright 2018 OpenSynergy Indonesia
+# Copyright 2020 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from openerp import models, fields, api
