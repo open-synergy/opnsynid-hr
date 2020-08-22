6fdf06940d32214c8833cc4b565a2404ef05571f
Andhitia Rama <andhitia.r@gmail.com>
2020-08-22 17:16:24 +0700

    [8.0.1.0.0] hr_timesheet_sheet_accrue_income

---
+++ hr_timesheet_sheet_accrue_income/__openerp__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 OpenSynergy Indonesia
+# Copyright 2020 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Generate Accrue Income Based On Timesheet Details - Sheet",
+    "version": "8.0.1.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "auto_install": True,
+    "depends": [
+        "hr_timesheet_accrue_income",
+        "hr_timesheet_sheet_onchange",
+        "base_action_rule",
+        "base_ir_filters_active",
+    ],
+    "data": [
+        "data/ir_filters_data.xml",
+        "data/ir_actions_server_data.xml",
+        "data/base_action_rule_data.xml",
+        "views/hr_timesheet_sheet_sheet_views.xml",
+    ],
+    "images": [
+        "static/description/banner.png",
+    ],
+}
