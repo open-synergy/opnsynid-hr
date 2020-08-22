2b3ff0db6d64c23852546be0ac99f6504f2d29ac
Andhitia Rama <andhitia.r@gmail.com>
2020-08-22 17:16:24 +0700

    [8.0.1.0.0] hr_timesheet_sheet_accrue_income

---
+++ hr_timesheet_sheet_accrue_income/models/hr_timesheet_sheet_sheet.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 OpenSynergy Indonesia
+# Copyright 2020 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, api
+
+
+class HrTimesheetSheetSheet(models.Model):
+    _name = "hr_timesheet_sheet.sheet"
+    _inherit = "hr_timesheet_sheet.sheet"
+
+    @api.multi
+    def _generate_accrue_income_move(self):
+        self.ensure_one()
+        obj_detail = self.env["hr.analytic.timesheet"]
+        criteria = [
+            ("sheet_id", "=", self.id),
+            ("accrue_income_move_id", "=", False),
+            ("accrue_income_ok", "=", True)
+        ]
+        details = obj_detail.search(criteria)
+        details.action_create_accrue_income_move()
+
+    @api.multi
+    def action_generate_accrue_income_move(self):
+        for document in self:
+            document._generate_accrue_income_move()
+
+    @api.multi
+    def _remove_accrue_income_move(self):
+        self.ensure_one()
+        obj_detail = self.env["hr.analytic.timesheet"]
+        criteria = [
+            ("sheet_id", "=", self.id),
+            ("accrue_income_move_id", "!=", False),
+        ]
+        details = obj_detail.search(criteria)
+        details.action_unlink_accrue_income_move()
+
+    @api.multi
+    def action_remove_accrue_income_move(self):
+        for document in self:
+            document._remove_accrue_income_move()
