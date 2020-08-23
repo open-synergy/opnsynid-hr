5638ba48a6739337a9113cc998b39c63792f5702
Andhitia Rama <andhitia.r@gmail.com>
2020-08-23 11:35:49 +0700

    [8.0.1.2.0] hr_timesheet_accrue_income

--- hr_timesheet_accrue_income/__openerp__.py
+++ hr_timesheet_accrue_income/__openerp__.py
@@ -4,7 +4,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Generate Accrue Income Based On Timesheet Details",
-    "version": "8.0.1.1.0",
+    "version": "8.0.1.2.0",
     "website": "https://simetri-sinergi.id",
     "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
     "license": "AGPL-3",
