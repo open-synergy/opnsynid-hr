a62478f28d23cae6391f9bc68a264b9821cf724a
mikevhe18 <viriyananda.michael@gmail.com>
2020-09-14 16:57:03 +0700

    Fixing Travis Error

--- hr_timesheet_sheet_multiple_approval/models/hr_timesheet_sheet_sheet.py
+++ hr_timesheet_sheet_multiple_approval/models/hr_timesheet_sheet_sheet.py
@@ -2,9 +2,7 @@
 # Copyright 2020 OpenSynergy Indonesia
 # Copyright 2020 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
-from openerp import models, api, _
-from openerp.exceptions import Warning as UserError
+from openerp import models, api
 
 
 class HrTimesheetSheetSheet(models.Model):
