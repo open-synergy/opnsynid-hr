393408880678345cb9d57b0b8672d0ac506075cc
mikevhe18 <viriyananda.michael@gmail.com>
2020-09-14 16:57:11 +0700

    Fixing Travis Error

--- hr_attendance_overtime_request/models/hr_overtime_request.py
+++ hr_attendance_overtime_request/models/hr_overtime_request.py
@@ -2,8 +2,7 @@
 # Copyright 2018 OpenSynergy Indonesia
 # Copyright 2020 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
-from openerp import models, fields, api, SUPERUSER_ID
+from openerp import models, fields, api
 from openerp.exceptions import Warning as UserError
 from openerp.tools.translate import _
 
--- hr_attendance_overtime_request/models/res_company.py
+++ hr_attendance_overtime_request/models/res_company.py
@@ -2,8 +2,7 @@
 # Copyright 2018 OpenSynergy Indonesia
 # Copyright 2020 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
-from openerp import models, fields, api
+from openerp import models, fields
 
 
 class ResCompany(models.Model):
