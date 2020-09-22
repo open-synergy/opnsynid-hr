fdcac7ee6e0dcef7450f9be0d774c576f30d39e2
mikevhe18 <viriyananda.michael@gmail.com>
2020-09-22 10:07:08 +0700

    [8.0.1.0.0][hr_payslip_multiple_approval]

---
+++ hr_payslip_multiple_approval/models/hr_payslip.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 OpenSynergy Indonesia
+# Copyright 2020 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+from openerp import models, api
+
+
+class HrPayslip(models.Model):
+    _name = "hr.payslip"
+    _inherit = [
+        "hr.payslip",
+        "tier.validation",
+    ]
+    _state_from = [
+        "draft",
+        "verify",
+    ]
+    _state_to = [
+        "done",
+    ]
+
+    @api.multi
+    def validate_tier(self):
+        _super = super(HrPayslip, self)
+        _super.validate_tier()
+        for document in self:
+            if document.validated:
+                document.signal_workflow("button_done")
+
+    @api.multi
+    def restart_validation(self):
+        _super = super(HrPayslip, self)
+        _super.restart_validation()
+        for document in self:
+            document.request_validation()
+
+    @api.multi
+    def hr_verify_sheet(self):
+        _super = super(HrPayslip, self)
+        _super.hr_verify_sheet()
+        for document in self:
+            document.request_validation()
--- hr_payslip_multiple_approval/models/tier_definition.py
+++ hr_payslip_multiple_approval/models/tier_definition.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-# Copyright 2018 OpenSynergy Indonesia
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
+# Copyright 2020 OpenSynergy Indonesia
+# Copyright 2020 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 from openerp import api, models
 
 
