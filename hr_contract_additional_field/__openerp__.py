4bdf8b02f3eb99b6aad2469a09fee00443c94ab1
Andhitia Rama <andhitia.r@gmail.com>
2020-10-09 04:19:08 +0700

    [hr_contract_additional_field] 8.0.1.4.0

--- hr_contract_additional_field/__openerp__.py
+++ hr_contract_additional_field/__openerp__.py
@@ -1,20 +1,26 @@
 # -*- coding: utf-8 -*-
 # Copyright 2018-2019 OpenSynergy Indonesia
+# Copyright 2020 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 # pylint: disable=locally-disabled, manifest-required-author
 {
     "name": "Additional Field for Employee Contract",
-    "version": "8.0.1.3.0",
+    "version": "8.0.1.4.0",
     "category": "Human Resource",
-    "website": "https://opensynergy-indonesia.com",
-    "author": "OpenSynergy Indonesia",
+    "website": "https://simetri-sinergi.id",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
     "license": "AGPL-3",
     "installable": True,
     "auto_install": True,
     "depends": [
         "hr_contract",
+        "hr_employee_employment_status",
     ],
     "data": [
         "views/hr_contract_views.xml",
+        "views/hr_contract_type_views.xml",
+    ],
+    "images": [
+        "static/description/banner.png",
     ],
 }
---
+++ hr_contract_additional_field/models/hr_contract_type.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018-2019 OpenSynergy Indonesia
+# Copyright 2020 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields
+
+
+class HrContractType(models.Model):
+    _name = "hr.contract.type"
+    _inherit = "hr.contract.type"
+
+    employment_status_id = fields.Many2one(
+        string="Employment Status",
+        comodel_name="hr.employment_status",
+    )
