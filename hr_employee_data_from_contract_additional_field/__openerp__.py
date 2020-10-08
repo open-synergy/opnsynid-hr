0283c1e3c86b7024119eb15f35cc701fbcfe27fe
Andhitia Rama <andhitia.r@gmail.com>
2020-10-09 04:27:35 +0700

    [hr_employee_data_from_contract_additional_field] 8.0.1.2.0

--- hr_employee_data_from_contract_additional_field/__openerp__.py
+++ hr_employee_data_from_contract_additional_field/__openerp__.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 # Copyright 2018 OpenSynergy Indonesia
+# Copyright 2020 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 # pylint: disable=locally-disabled, manifest-required-author
 {
     "name": "Employee Data From Contract Additional Field",
-    "version": "8.0.1.1.0",
+    "version": "8.0.1.2.0",
     "category": "Human Resource",
-    "website": "https://opensynergy-indonesia.com",
-    "author": "OpenSynergy Indonesia",
+    "website": "https://simetri-sinergi.id",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
     "license": "AGPL-3",
     "installable": True,
     "depends": [
@@ -15,5 +16,8 @@
         "hr_employee_data_from_contract",
     ],
     "data": [
+    ],
+    "images": [
+        "static/description/banner.png",
     ],
 }
--- hr_employee_data_from_contract_additional_field/models/hr_employee.py
+++ hr_employee_data_from_contract_additional_field/models/hr_employee.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # Copyright 2018 OpenSynergy Indonesia
+# Copyright 2020 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from openerp import models, fields
@@ -18,14 +19,21 @@ class HrEmployee(models.Model):
     parent_id = fields.Many2one(
         string="Manager",
         comodel_name="hr.employee",
-        related="current_contract_id.contract_department_id.manager_id",
+        related="current_contract_id.manager_id",
         store=True,
         readonly=True,
     )
     company_id = fields.Many2one(
         string="Company",
         comodel_name="res.company",
-        related="contract_id.company_id",
+        related="current_contract_id.company_id",
+        store=True,
+        readonly=True,
+    )
+    employment_status_id = fields.Many2one(
+        string="Employment Status",
+        comodel_name="hr.employment_status",
+        related="current_contract_id.employment_status_id",
         store=True,
         readonly=True,
     )
