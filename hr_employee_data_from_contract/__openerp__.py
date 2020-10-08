979136a9228aad742ee70fe90f43e4815ab71eba
Andhitia Rama <andhitia.r@gmail.com>
2020-10-09 04:24:06 +0700

    [hr_employee_data_from_contract] 8.0.1.2.0

--- hr_employee_data_from_contract/__openerp__.py
+++ hr_employee_data_from_contract/__openerp__.py
@@ -1,18 +1,23 @@
 # -*- coding: utf-8 -*-
 # Copyright 2018 OpenSynergy Indonesia
+# Copyright 2020 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 # pylint: disable=locally-disabled, manifest-required-author
 {
     "name": "Employee Data From Contract",
-    "version": "8.0.1.1.0",
+    "version": "8.0.1.2.0",
     "category": "Human Resource",
-    "website": "https://opensynergy-indonesia.com",
-    "author": "OpenSynergy Indonesia",
+    "website": "https://simetri-sinergi.id",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
     "license": "AGPL-3",
     "installable": True,
     "depends": [
         "hr_contract",
     ],
     "data": [
+        "views/hr_employee_views.xml",
+    ],
+    "images": [
+        "static/description/banner.png",
     ],
 }
