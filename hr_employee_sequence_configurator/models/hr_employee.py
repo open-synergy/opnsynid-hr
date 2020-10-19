46a57c1958ad0db93821cf53ef2595a57b2bd418
mikevhe18 <viriyananda.michael@gmail.com>
2020-10-19 18:17:15 +0700

    [8.0.1.0.0][hr_employee_sequence_configurator]

---
+++ hr_employee_sequence_configurator/models/hr_employee.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 OpenSynergy Indonesia
+# Copyright 2020 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import models, fields, api
+
+
+class HrEmployee(models.Model):
+    _name = "hr.employee"
+    _inherit = [
+        "hr.employee",
+        "base.sequence_document",
+    ]
+
+    employee_code = fields.Char(
+        string="Employee Code",
+        default="/",
+        required=True,
+        copy=False,
+    )
+
+    @api.multi
+    def action_create_identification_id(self):
+        for document in self:
+            document._create_identification_id()
+
+    @api.multi
+    def _create_identification_id(self):
+        self.ensure_one()
+        sequence = self._create_sequence()
+        self.write({
+            "employee_code": sequence,
+        })
---
+++ hr_employee_sequence_configurator/models/hr_employment_status.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import models, fields
+
+
+class HrEmploymentStatus(models.Model):
+    _inherit = "hr.employment_status"
+
+    sequence_id = fields.Many2one(
+        string="Sequence",
+        comodel_name="ir.sequence",
+        company_dependent=True,
+    )
