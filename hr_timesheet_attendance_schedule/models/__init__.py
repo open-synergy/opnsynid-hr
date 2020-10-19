10bfabfd8581eb55e4729ef55199a20f39b90ffe
mikevhe18 <viriyananda.michael@gmail.com>
2020-10-19 12:29:35 +0700

    [8.0.4.0.0][hr_timesheet_attendance_schedule]

--- hr_timesheet_attendance_schedule/models/__init__.py
+++ hr_timesheet_attendance_schedule/models/__init__.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 # Copyright 2018 OpenSynergy Indonesia
+# Copyright 2020 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
-from . import hr_attendance
-from . import hr_timesheet_attendance_schedule
-from . import hr_timesheet_sheet
-from . import res_company
-from . import res_config
-from . import hr_timesheet_attendance_schedule_change
+from . import (
+    hr_attendance,
+    hr_timesheet_attendance_schedule,
+    hr_timesheet_sheet,
+    res_company,
+    res_config,
+    hr_timesheet_attendance_schedule_change,
+    tier_definition,
+)
--- hr_timesheet_attendance_schedule/models/res_company.py
+++ hr_timesheet_attendance_schedule/models/res_company.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # Copyright 2018 OpenSynergy Indonesia
+# Copyright 2020 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from openerp import models, fields
@@ -18,31 +19,31 @@ class ResCompany(models.Model):
         default=0.0,
         required=True,
     )
-    schedule_change_confirm_grp_ids = fields.Many2one(
+    schedule_change_confirm_grp_ids = fields.Many2many(
         string="Allowed To Confirm Attendance Schedule Change",
         comodel_name="res.groups",
         relation="rel_company_2_group_schedule_change_confirm",
         column1="company_id",
         column2="group_id",
     )
-    schedule_change_approve_grp_ids = fields.Many2one(
-        string="Allowed To Approve Attendance Schedule Change",
-        comodel_name="res.groups",
-        relation="rel_company_2_group_schedule_change_approve",
-        column1="company_id",
-        column2="group_id",
-    )
-    schedule_change_cancel_grp_ids = fields.Many2one(
+    schedule_change_cancel_grp_ids = fields.Many2many(
         string="Allowed To Cancel Attendance Schedule Change",
         comodel_name="res.groups",
         relation="rel_company_2_group_schedule_change_cancel",
         column1="company_id",
         column2="group_id",
     )
-    schedule_change_restart_grp_ids = fields.Many2one(
+    schedule_change_restart_grp_ids = fields.Many2many(
         string="Allowed To Approve Attendance Schedule Change",
         comodel_name="res.groups",
         relation="rel_company_2_group_schedule_change_restart",
+        column1="company_id",
+        column2="group_id",
+    )
+    schedule_change_restart_val_grp_ids = fields.Many2many(
+        string="Allow To Restart Approval",
+        comodel_name="res.groups",
+        relation="rel_company_2_group_schedule_change_restart_val",
         column1="company_id",
         column2="group_id",
     )
