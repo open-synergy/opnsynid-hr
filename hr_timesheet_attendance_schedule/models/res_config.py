5410d9356efc835fbaa7f820b2609196b46061ad
mikevhe18 <viriyananda.michael@gmail.com>
2020-10-19 12:29:35 +0700

    [hr_timesheet_attendance_schedule] 8.0.4.0.0
    * Add dependency to base_action_rule
    * Branding
    * Python reformat
    * Fix timesheet change workflow policy use m2m

--- hr_timesheet_attendance_schedule/models/res_config.py
+++ hr_timesheet_attendance_schedule/models/res_config.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # Copyright 2018 OpenSynergy Indonesia
+# Copyright 2020 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from openerp import models, fields
@@ -16,23 +17,23 @@ class ResConfig(models.TransientModel):
         string="Late Attendance Buffer",
         related="company_id.late_attendance_buffer",
     )
-    schedule_change_confirm_grp_ids = fields.Many2one(
+    schedule_change_confirm_grp_ids = fields.Many2many(
         string="Allowed To Confirm Attendance Schedule Change",
         comodel_name="res.groups",
         related="company_id.schedule_change_confirm_grp_ids",
     )
-    schedule_change_approve_grp_ids = fields.Many2one(
-        string="Allowed To Approve Attendance Schedule Change",
-        comodel_name="res.groups",
-        related="company_id.schedule_change_approve_grp_ids",
-    )
-    schedule_change_cancel_grp_ids = fields.Many2one(
+    schedule_change_cancel_grp_ids = fields.Many2many(
         string="Allowed To Cancel Attendance Schedule Change",
         comodel_name="res.groups",
         related="company_id.schedule_change_cancel_grp_ids",
     )
-    schedule_change_restart_grp_ids = fields.Many2one(
+    schedule_change_restart_grp_ids = fields.Many2many(
         string="Allowed To Restart Attendance Schedule Change",
         comodel_name="res.groups",
         related="company_id.schedule_change_restart_grp_ids",
+    )
+    schedule_change_restart_val_grp_ids = fields.Many2many(
+        string="Allowed To Restart Approval",
+        comodel_name="res.groups",
+        related="company_id.schedule_change_restart_val_grp_ids",
     )
