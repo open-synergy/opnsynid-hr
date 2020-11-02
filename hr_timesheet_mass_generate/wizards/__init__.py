e81c52ab6768e2a2a2b1ed017d6c8c2726354862
mikevhe18 <viriyananda.michael@gmail.com>
2020-11-02 10:23:10 +0700

    [8.0.1.0.1][hr_timesheet_mass_generate]

--- hr_timesheet_mass_generate/wizards/__init__.py
+++ hr_timesheet_mass_generate/wizards/__init__.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 # Copyright 2017 OpenSynergy Indonesia
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
-from . import hr_timesheet_mass_generate
+# Copyright 2020 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from . import (
+    hr_timesheet_mass_generate,
+)
--- hr_timesheet_mass_generate/wizards/hr_timesheet_mass_generate.py
+++ hr_timesheet_mass_generate/wizards/hr_timesheet_mass_generate.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # Copyright 2017 OpenSynergy Indonesia
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# Copyright 2020 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from openerp import models, fields, api, _
 from datetime import datetime
@@ -42,40 +43,53 @@ class HrTimesheetMassGenerate(models.TransientModel):
                 raise ValidationError(strWarning)
 
     @api.multi
+    def _prepare_data_timesheet(self, employee):
+        data = {}
+        if self._check_data(employee):
+            data = {
+                "employee_id": employee.id,
+                "date_from": self.date_from,
+                "date_to": self.date_to
+            }
+        return data
+
+    @api.multi
+    def _check_data(self, employee):
+        obj_hr_timesheet_sheet =\
+            self.env["hr_timesheet_sheet.sheet"]
+
+        if not employee.user_id:
+            raise ValidationError(
+                _("Employee {name} must be linked to a user.".format(
+                    name=employee.name)))
+
+        if not employee.journal_id:
+            raise ValidationError(
+                _("Employee {name} must have analytic journal.".format(
+                    name=employee.name)))
+
+        criteria = [
+            ("employee_id", "=", employee.id),
+            ("date_from", "<=", self.date_from),
+            ("date_to", ">=", self.date_to)
+        ]
+
+        timesheet_sheet =\
+            obj_hr_timesheet_sheet.search(criteria)
+
+        if timesheet_sheet:
+            raise ValidationError(
+                _("Timesheet already exists for {name}.".format(
+                    name=employee.name)))
+        return True
+
+    @api.multi
     def button_generate(self):
         obj_hr_timesheet_sheet =\
             self.env["hr_timesheet_sheet.sheet"]
 
         for employee in self.employee_ids:
-
-            if not employee.user_id:
-                raise ValidationError(
-                    _("Employee {name} must be linked to a user.".format(
-                        name=employee.name)))
-            elif not employee.journal_id:
-                raise ValidationError(
-                    _("Employee {name} must have analytic journal.".format(
-                        name=employee.name)))
-            else:
-                criteria = [
-                    ("employee_id", "=", employee.id),
-                    ("date_from", "<=", self.date_from),
-                    ("date_to", ">=", self.date_to)
-                ]
-
-                timesheet_sheet =\
-                    obj_hr_timesheet_sheet.search(criteria)
-
-                if timesheet_sheet:
-                    raise ValidationError(
-                        _("Timesheet already exists for {name}.".format(
-                            name=employee.name)))
-                else:
-                    value = {
-                        "employee_id": employee.id,
-                        "date_from": self.date_from,
-                        "date_to": self.date_to
-                    }
-                    obj_hr_timesheet_sheet.create(value)
+            data = self._prepare_data_timesheet(employee)
+            obj_hr_timesheet_sheet.create(data)
 
         return {"type": "ir.actions.act_window_close"}
