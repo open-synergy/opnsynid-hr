5c5454075d4abc38a5878461a026b5b45a84837c
azmimr67@gmail.com <azmimr67@gmail.com>
2021-01-22 16:15:52 +0700

    [hr_holiday_dashboard_tile] 8.0.1.0.0

---
+++ hr_holiday_dashboard_tile/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 OpenSynergy Indonesia
+# Copyright 2020 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
---
+++ hr_holiday_dashboard_tile/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 OpenSynergy Indonesia
+# Copyright 2020 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "HR Holiday - Dashboard and Tile",
+    "version": "8.0.1.0.0",
+    "category": "Human Resources",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "web_dashboard_tile",
+        "hr_holidays",
+    ],
+    "data": [
+        "data/dashboard_tile_hr_holiday_data.xml",
+    ],
+}
