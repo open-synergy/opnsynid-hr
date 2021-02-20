6af8271d70351877d5ef2f451d52df60129b826a
Andhitia Rama <andhitia.r@gmail.com>
2021-02-20 21:38:36 +0700

    [hr_timesheet_accrue_income] 8.0.1.2.2
    * Add analytic_account_id for each account.move.line created

--- hr_timesheet_accrue_income/__openerp__.py
+++ hr_timesheet_accrue_income/__openerp__.py
@@ -4,7 +4,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Generate Accrue Income Based On Timesheet Details",
-    "version": "8.0.1.2.1",
+    "version": "8.0.1.2.2",
     "website": "https://simetri-sinergi.id",
     "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
     "license": "AGPL-3",
