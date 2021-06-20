76fbb8713b8c59b848f53655d9c22182bd0ade86
Nurazmi <azmimr67@gmail.com>
2021-06-20 19:15:22 +0700

    [hr_timesheet_task_different_analytic] 8.0.1.1.0
    Fix #257

--- hr_timesheet_task_different_analytic/__openerp__.py
+++ hr_timesheet_task_different_analytic/__openerp__.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
 # Copyright 2020 OpenSynergy Indonesia
 # Copyright 2020 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Choose Different Analytic Account for Task",
-    "version": "8.0.1.0.0",
+    "version": "8.0.1.1.0",
     "website": "https://simetri-sinergi.id",
     "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
     "license": "AGPL-3",
