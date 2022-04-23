32605beebed1e61579a20e6b4928dba0ba92e02f
Andhitia Rama <andhitia.r@gmail.com>
2022-04-23 14:01:24 +0700

    [ssi_hr] 14.0.1.0.0

---
+++ ssi_hr/__manifest__.py
@@ -0,0 +1,26 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Human Resource",
+    "version": "14.0.1.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "license": "LGPL-3",
+    "installable": True,
+    "application": True,
+    "auto_install": True,
+    "depends": [
+        "hr",
+        "configuration_helper",
+    ],
+    "data": [
+        "security/ir_module_category_data.xml",
+        "security/res_group_data.xml",
+        "security/ir.model.access.csv",
+        "menu.xml",
+        "views/res_config_settings_views.xml",
+    ],
+    "demo": [],
+}
---
+++ ssi_hr/models/res_company.py
@@ -0,0 +1,10 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import models
+
+
+class ResCompany(models.Model):
+    _name = "res.company"
+    _inherit = "res.company"
