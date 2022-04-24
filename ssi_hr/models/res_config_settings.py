bffca7b43847d72c926cb1908d29481d5e73193e
Andhitia Rama <andhitia.r@gmail.com>
2022-04-24 13:10:55 +0700

    [ssi_hr] 14.0.2.0.0

--- ssi_hr/models/res_config_settings.py
+++ ssi_hr/models/res_config_settings.py
@@ -2,7 +2,7 @@
 # Copyright 2022 PT. Simetri Sinergi Indonesia
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-from odoo import models
+from odoo import fields, models
 
 
 class ResConfigSettings(models.TransientModel):
@@ -11,3 +11,52 @@ class ResConfigSettings(models.TransientModel):
         "res.config.settings",
         "abstract.config.settings",
     ]
+
+    module_ssi_hr_join_transition = fields.Boolean(
+        string="Join Transition",
+    )
+    module_ssi_hr_promotion_transition = fields.Boolean(
+        string="Promotion Transition",
+    )
+    module_ssi_hr_mutation_transition = fields.Boolean(
+        string="Mutation Transition",
+    )
+    module_ssi_hr_demotion_transition = fields.Boolean(
+        string="Demotion Transition",
+    )
+    module_ssi_hr_termination_transition = fields.Boolean(
+        string="Termination Transition",
+    )
+    module_ssi_hr_new_assignment_transition = fields.Boolean(
+        string="New Assignment Transition",
+    )
+    module_ssi_hr_award = fields.Boolean(
+        string="Award",
+    )
+    module_ssi_hr_dicipline = fields.Boolean(
+        string="Dicipline",
+    )
+    module_ssi_hr_timesheet_attendance = fields.Boolean(
+        string="Attendance",
+    )
+    module_ssi_hr_timesheet_worklog = fields.Boolean(
+        string="Worklog",
+    )
+    module_ssi_hr_holiday = fields.Boolean(
+        string="Holiday",
+    )
+    module_ssi_hr_overtime = fields.Boolean(
+        string="Overtime",
+    )
+    module_ssi_hr_payroll = fields.Boolean(
+        string="Payroll",
+    )
+    module_ssi_hr_reimbursement = fields.Boolean(
+        string="Reimbursement",
+    )
+    module_ssi_hr_cash_advance = fields.Boolean(
+        string="Cash Advance",
+    )
+    module_ssi_hr_expense_account = fields.Boolean(
+        string="Expense Account",
+    )
