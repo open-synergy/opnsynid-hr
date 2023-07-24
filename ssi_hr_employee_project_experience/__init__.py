63d312c4a48ce9f14cd53e85c7a836b2072d3a67
mikevhe18 <viriyananda.michael@gmail.com>
2023-07-24 13:03:09 +0700

    [ssi_hr_employee_project_experience] Change License to AGPL-3

--- ssi_hr_employee_project_experience/__init__.py
+++ ssi_hr_employee_project_experience/__init__.py
@@ -1,6 +1,6 @@
 # Copyright 2023 OpenSynergy Indonesia
 # Copyright 2023 PT. Simetri Sinergi Indonesia
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import (
     models,
