760d6dc7ce25b223a6f5e723222f780be0248498
mikevhe18 <viriyananda.michael@gmail.com>
2023-07-24 12:51:57 +0700

    Change License to AGPL-3

--- ssi_hr/__manifest__.py
+++ ssi_hr/__manifest__.py
@@ -1,13 +1,13 @@
 # Copyright 2022 OpenSynergy Indonesia
 # Copyright 2022 PT. Simetri Sinergi Indonesia
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Human Resource",
     "version": "14.0.2.5.0",
     "website": "https://simetri-sinergi.id",
     "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
-    "license": "LGPL-3",
+    "license": "AGPL-3",
     "installable": True,
     "application": True,
     "auto_install": True,
