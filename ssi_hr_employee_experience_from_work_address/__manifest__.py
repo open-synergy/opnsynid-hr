d990402da8ed57215c510ef2a4527babc199f5a7
mikevhe18 <viriyananda.michael@gmail.com>
2023-08-15 15:40:12 +0700

    [ssi_hr_employee_experience_from_work_address] 14.0.1.0.0

---
+++ ssi_hr_employee_experience_from_work_address/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2023 OpenSynergy Indonesia
+# Copyright 2023 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Employee Experience From Home Address",
+    "version": "14.0.1.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "auto_install": True,
+    "depends": [
+        "ssi_partner_experience",
+        "ssi_hr",
+    ],
+    "data": [
+        "views/hr_employee_views.xml",
+    ],
+    "demo": [],
+}
