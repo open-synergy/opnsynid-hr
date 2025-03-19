a4a97bac89d4896a4f24d0e24d85478c4a8f075a
mikevhe18 <viriyananda.michael@gmail.com>
2025-03-19 13:59:54 +0700

    [CI] C8101

--- ssi_hr_employee_personal_from_work_address/__manifest__.py
+++ ssi_hr_employee_personal_from_work_address/__manifest__.py
@@ -1,7 +1,7 @@
 # Copyright 2024 OpenSynergy Indonesia
 # Copyright 2024 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
+# pylint: disable=C8101
 {
     "name": "Employee Personal Information From Home Address",
     "version": "14.0.1.2.0",
