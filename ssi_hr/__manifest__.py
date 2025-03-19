f45b82e1ae46e62f6abc91354178b9d2a86f7e99
mikevhe18 <viriyananda.michael@gmail.com>
2025-03-19 14:02:22 +0700

    [CI] C8101

--- ssi_hr/__manifest__.py
+++ ssi_hr/__manifest__.py
@@ -1,7 +1,7 @@
 # Copyright 2022 OpenSynergy Indonesia
 # Copyright 2022 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
+# pylint: disable=C8101
 {
     "name": "Human Resource",
     "version": "14.0.2.7.0",
