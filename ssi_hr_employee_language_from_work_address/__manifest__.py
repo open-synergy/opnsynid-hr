b50c9c3c98740f9155f333f3ddce3ff076632217
mikevhe18 <viriyananda.michael@gmail.com>
2025-03-19 15:27:57 +0700

    [ssi_hr_employee_language_from_work_address] 14.0.1.0.1

--- ssi_hr_employee_language_from_work_address/__manifest__.py
+++ ssi_hr_employee_language_from_work_address/__manifest__.py
@@ -4,7 +4,7 @@
 # pylint: disable=C8101
 {
     "name": "Employee Language From Home Address",
-    "version": "14.0.1.0.0",
+    "version": "14.0.1.0.1",
     "website": "https://simetri-sinergi.id",
     "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
     "license": "AGPL-3",
