b0e9d5b6ebbc3130e764fc0fcd698c3644d24de6
mikevhe18 <viriyananda.michael@gmail.com>
2025-03-19 13:59:07 +0700

    [CI] C8101

--- ssi_hr_employee_experience_from_work_address/__manifest__.py
+++ ssi_hr_employee_experience_from_work_address/__manifest__.py
@@ -1,7 +1,7 @@
 # Copyright 2023 OpenSynergy Indonesia
 # Copyright 2023 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
+# pylint: disable=C8101
 {
     "name": "Employee Experience From Home Address",
     "version": "14.0.1.1.0",
