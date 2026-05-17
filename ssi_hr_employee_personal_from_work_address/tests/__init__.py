9ae2ec67eb4ab1a6eca8b9c96f61ee52b590ac9f
Andhitia Rama <andhitia.r@gmail.com>
2026-05-17 08:07:50 +0700

    [UPD] ssi_hr_employee_personal_from_work_address
    * Add unit tests

---
+++ ssi_hr_employee_personal_from_work_address/tests/__init__.py
@@ -0,0 +1,5 @@
+# Copyright 2025 OpenSynergy Indonesia
+# Copyright 2025 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import test_ssi_hr_employee_personal_from_work_address  # noqa: F401
